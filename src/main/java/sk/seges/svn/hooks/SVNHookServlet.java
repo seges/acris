97451894e3de840170ae3debed7f474e5c023ad5
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2009-12-04 15:24:51 +0000

    Added web SVN post-commit hook. If you can see this text in your mail box, then it is working :)
    git-svn-id: https://acris.googlecode.com/svn/branches/svn-post-commit@40 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ src/main/java/sk/seges/svn/hooks/SVNHookServlet.java
@@ -0,0 +1,144 @@
+package sk.seges.svn.hooks;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.security.KeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Properties;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.codec.binary.Hex;
+
+import sk.seges.svn.json.reader.PayloadJSONReader;
+import sk.seges.svn.mail.MailConfiguration;
+import sk.seges.svn.mail.SVNMailSender;
+
+public class SVNHookServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7084142257979312491L;
+
+	// Stores the contents of the JSON object sent in the message from Google
+	// Code
+	private String payload;
+
+	// Stores the hash value sent in the request headers from Google Code
+	private String expectedHash;
+
+	// The javax.crypto.SecretKey used to seed the HMAC-MD5 message
+	private SecretKey secret;
+
+	public SVNHookServlet() throws IOException {
+		super();
+		// Assumption: Your Google Code project's secret key is stored in the
+		// file referenced in the File() constructor below
+		secret = new GoogleCodeSecretKey("DfowgMGgc7ZoBVJx");
+		payload = "";
+		expectedHash = null;
+	}
+
+	/*
+	 * (non-Java-doc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request,
+	 * HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		BufferedReader r = request.getReader();
+		String line;
+		while ((line = r.readLine()) != null)
+			payload = payload.concat(line + "\n");
+		r.close();
+		payload = payload.trim();
+
+		expectedHash = request
+			.getHeader("Google-Code-Project-Hosting-Hook-Hmac");
+
+//		payload = "{\"repository_path\":\"http://acris.googlecode.com/svn/\",\"project_name\":\"acris\",\"revisions\":[{\"added\":[],\"author\":\"simun@seges.sk\",\"url\":\"http://acris.googlecode.com/svn-history/r34/\",\"timestamp\":1259923581,\"modified\":[\"/test/test.file\"],\"path_count\":1,\"message\":\"test\",\"removed\":[],\"revision\":34}],\"revision_count\":1}";
+		
+		// The expected hash result, as sent in the request headers by Google
+		expectedHash = request
+				.getHeader("Google-Code-Project-Hosting-Hook-Hmac");
+
+		// Validate the request and write the results; you'll want to do
+		// something
+		// much more exciting based on the results of the validate() method
+		String result;
+		if (validate()) {
+			MailConfiguration mailConfiguration = new MailConfiguration(getServletContext());
+			
+			PayloadJSONReader payloadJSONReader = new PayloadJSONReader();
+			SVNMailSender mailSender = new SVNMailSender(mailConfiguration, payloadJSONReader.readFromJSON(payload)); 
+			mailSender.sendMail();
+
+			result = "Message authenticated successfully!";
+		} else {
+			Properties props = new Properties();
+	        Session session = Session.getDefaultInstance(props, null);
+
+	        try {
+		        Message msg = new MimeMessage(session);
+	            msg.setFrom(new InternetAddress("simun@seges.sk"));
+	            msg.addRecipient(Message.RecipientType.TO,
+	                             new InternetAddress("peter.simun@saf-ag.com"));
+	            msg.setSubject("Wrong auth");
+	            msg.setText("Message authentication failed!");
+	            Transport.send(msg);
+	        } catch (Exception e) {
+	        	throw new RuntimeException(e);
+	        }
+	        
+            result = "Message authentication failed!";
+		}
+		response.getWriter().write(result);
+		response.getWriter().close();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+	
+	/**
+	 * Compute the payload's HMAC-MD5 hash value and compare it to the expected
+	 * value received in request headers from Google
+	 * 
+	 * @return True if the payload's hash value is equal to the expected value
+	 *         stored as a member object of this instance
+	 */
+	private boolean validate() {
+		if (expectedHash == null)
+			return false;
+
+		try {
+			Mac mac = Mac.getInstance("HmacMD5");
+			mac.init(secret);
+			// I'm using Apache Commons codec lib to do the hex encoding; use
+			// whatever hex encoder lib you like
+			return expectedHash.equals(new String(Hex.encodeHex(mac.doFinal(payload.getBytes(Charset.forName("UTF-8"))))));
+		} catch (KeyException e) {
+			e.printStackTrace(); // Presumably, this stack trace will be dumped
+									// to your J2EE container's log files
+			return false;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
---
+++ src/main/java/sk/seges/svn/mail/SVNMailSender.java
@@ -0,0 +1,78 @@
+package sk.seges.svn.mail;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import sk.seges.svn.json.data.OverallInfo;
+import sk.seges.svn.json.data.RevisionInfo;
+
+public class SVNMailSender {
+
+	private OverallInfo commitInfo;
+	private MailConfiguration mailConfiguration;
+	
+	public SVNMailSender(MailConfiguration mailConfiguration, OverallInfo commitInfo) {
+		this.commitInfo = commitInfo;
+		this.mailConfiguration = mailConfiguration;
+	}
+	
+	public void sendMail() {
+		List<String> mailAddresses = mailConfiguration.getMailAddresses();
+		sendMail(mailAddresses);
+	}
+
+	private void sendMail(List<String> mails) {
+		String lineSeparator = "\n";
+
+		Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+		for (RevisionInfo revisionInfo : commitInfo.getRevisions()) {
+			String body = "";
+			body += "Repository: " + commitInfo.getRepositoryPath() + lineSeparator;
+			body += "Revision: " + revisionInfo.getRevisionNr() + lineSeparator;
+			body += "Author: " + revisionInfo.getAuthor() + lineSeparator;
+			body += "" + lineSeparator;
+			
+			Date date = new Date();
+			date.setTime(revisionInfo.getTimestamp());
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z (EEE, dd MMM yyyy)");
+			
+			body += "Date: " + dateFormat.format(date) + lineSeparator + lineSeparator;
+			body += "Log msg: " + revisionInfo.getMessage() + lineSeparator;
+			body += "" + lineSeparator;
+			body += "Changes:" + lineSeparator;
+
+			for (String change : revisionInfo.getAddedPaths()) {
+				body += "A " + change + lineSeparator;
+			}
+			for (String change : revisionInfo.getModifiedPaths()) {
+				body += "U " + change + lineSeparator;
+			}
+			for (String change : revisionInfo.getRemovedPaths()) {
+				body += "D " + change + lineSeparator;
+			}
+
+			try {
+	            Message msg = new MimeMessage(session);
+	            msg.setFrom(new InternetAddress("devel@seges.sk"));
+	            for (String mail : mails) 
+		            msg.addRecipient(Message.RecipientType.TO,
+		                             new InternetAddress(mail));
+	            msg.setSubject("[SVN - " + commitInfo.getRepositoryPath() + "]: " + revisionInfo.getMessage());
+	            msg.setText(body);
+	            Transport.send(msg);
+	        } catch (Exception e) {
+	        	throw new RuntimeException(e);
+	        }
+		}
+	}
+}
