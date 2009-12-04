d741fd823586c103b0fda56df10182c6eee1ed0f
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2009-12-04 15:24:51 +0000

    Added web SVN post-commit hook. If you can see this text in your mail box, then it is working :)
    git-svn-id: https://acris.googlecode.com/svn@40 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ branches/svn-post-commit/src/main/java/sk/seges/svn/mail/MailConfiguration.java
@@ -0,0 +1,54 @@
+package sk.seges.svn.mail;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
+
+public class MailConfiguration {
+	private List<String> mailAddress = new ArrayList<String>();
+	private String fromMail;
+	
+	public MailConfiguration(ServletContext context) {
+		
+		Properties properties;
+		try {
+			properties = new Properties();
+			URL url = context.getResource("/WEB-INF/settings.properties");
+			properties.load(new FileInputStream(new File(url.getFile())));
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		String from = (String)properties.get("from");
+		mailAddress.add(from);
+
+		String to = (String)properties.get("to");
+		
+		String[] mails = to.split(";");
+		
+		for (String mail: mails) {
+			mailAddress.add(mail);
+		}
+		
+//		mailAddress.add("simun@seges.sk");
+//		mailAddress.add("gazo@seges.sk");
+//		mailAddress.add("grosos@seges.sk");
+//		mailAddress.add("sivak@seges.sk");
+	}
+	
+	public List<String> getMailAddresses() {
+		return mailAddress;
+	}
+	
+	public String getFromMail() {
+		return fromMail;
+	}
+}
