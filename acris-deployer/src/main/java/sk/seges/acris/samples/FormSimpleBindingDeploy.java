156a300fa155d5e969f6f1fec2b81d7604dcc3f1
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2009-12-07 19:45:17 +0000

    Added acris App engine deployer application
    git-svn-id: https://acris.googlecode.com/svn/trunk@71 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ acris-deployer/src/main/java/sk/seges/acris/samples/FormSimpleBindingDeploy.java
@@ -0,0 +1,13 @@
+package sk.seges.acris.samples;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+public class FormSimpleBindingDeploy extends FormSimpleBinding implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		super.onModuleLoad();
+	}
+	
+}
