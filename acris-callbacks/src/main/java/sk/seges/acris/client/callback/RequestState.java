65b0c41c75ef8537c33fadaea128a2db048af7a7
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2010-01-07 17:09:46 +0000

    Added acris-callbacks
    git-svn-id: https://acris.googlecode.com/svn/trunk@108 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ acris-callbacks/src/main/java/sk/seges/acris/client/callback/RequestState.java
@@ -0,0 +1,12 @@
+package sk.seges.acris.client.callback;
+
+/**
+ * State of the request associated with {@link TrackingAsyncCallback}
+ * 
+ * @author fat
+ */
+public enum RequestState {
+	REQUEST_SUCCESS,
+
+	REQUEST_FAILURE;
+}
