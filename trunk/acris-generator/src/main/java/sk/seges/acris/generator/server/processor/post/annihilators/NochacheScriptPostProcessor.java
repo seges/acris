f364b7d6c7b17192bdccf72a0d92152b464ff1dd
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2010-02-24 16:58:33 +0000

    Added annihilator for Nocache
    git-svn-id: https://acris.googlecode.com/svn@618 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ trunk/acris-generator/src/main/java/sk/seges/acris/generator/server/processor/post/annihilators/NochacheScriptPostProcessor.java
@@ -0,0 +1,21 @@
+package sk.seges.acris.generator.server.processor.post.annihilators;
+
+import org.htmlparser.Node;
+import org.htmlparser.tags.BodyTag;
+import org.htmlparser.tags.ScriptTag;
+
+public class NochacheScriptPostProcessor extends AbstractPostProcessorAnnihilator {
+
+	protected boolean supportsParent(Node node) {
+		return (node instanceof BodyTag);	
+	}
+
+	protected boolean supportsNode(Node node) {
+		if (node instanceof ScriptTag) {
+			String jsSource = ((ScriptTag)node).getAttribute("src");
+			return (jsSource != null && jsSource.toLowerCase().indexOf(".nocache.js") > 0); 
+		}
+		
+		return false;
+	}
+}
