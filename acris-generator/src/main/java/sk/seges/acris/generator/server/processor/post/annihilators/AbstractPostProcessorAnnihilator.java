594719dd5ee48a2bf8e882b3316f51516ceb6876
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2010-02-24 16:58:33 +0000

    Added annihilator for Nocache
    git-svn-id: https://acris.googlecode.com/svn/trunk@618 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ acris-generator/src/main/java/sk/seges/acris/generator/server/processor/post/annihilators/AbstractPostProcessorAnnihilator.java
@@ -0,0 +1,47 @@
+package sk.seges.acris.generator.server.processor.post.annihilators;
+
+import org.htmlparser.Node;
+import org.htmlparser.util.NodeList;
+
+import sk.seges.acris.generator.server.processor.post.AbstractElementPostProcessor;
+
+public abstract class AbstractPostProcessorAnnihilator extends AbstractElementPostProcessor {
+
+	protected abstract boolean supportsParent(Node node);
+	protected abstract boolean supportsNode(Node node);
+	
+	
+	@Override
+	public boolean supports(Node node) {
+		return supportsParent(node);
+	}
+
+	@Override
+	public boolean process(Node node) {
+
+		NodeList nodeList = node.getChildren();
+
+		int size = nodeList.size();
+		
+		int removeIndex = -1;
+		
+		for (int i = 0; i < size; i++) {
+		
+			Node childNode = nodeList.elementAt(i);
+			
+			if (supportsNode(childNode)) {
+				//amen
+				removeIndex = i;
+				break;
+			}
+		}
+		
+		if (removeIndex == -1) {
+			return false;
+		}
+
+		nodeList.remove(removeIndex);
+		
+		return true;
+	}
+}
