f3c6bf421e7aa8872ca64d84b82d15b6b1d260b3
ladislav.gazo <ladislav.gazo@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2010-06-16 07:17:41 +0000

    secured menu with rebuild event + moved chocolate event to acris
    git-svn-id: https://acris.googlecode.com/svn/trunk@850 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-common/src/main/java/sk/seges/acris/constant/PanelRef.java
+++ acris-common/src/main/java/sk/seges/acris/constant/PanelRef.java
@@ -8,6 +8,7 @@ public interface PanelRef {
     public static final String FLOW_LAYOUT_PANEL = "flowLP";
     public static final String DECK_LAYOUT_PANEL = "deckLP";
     public static final String MENU_LAYOUT_PANEL = "menuLP";
+    public static final String SECURED_MENU_LAYOUT_PANEL = "secMenuLP";
 	public static final String UNAVAILABLE_PANEL = "unavailableP";
 	public static final String POPOUT_LOGIN_PANEL = "popoutLoginP";
 	public static final String LANGUAGE_SELECTOR_PANEL = "langSelPanel";
