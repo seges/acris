1b3b063b616350133ccacbcbc93af7581d3fb9e4
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2010-09-06 15:04:11 +0000

    creating structure in repo
    git-svn-id: https://acris.googlecode.com/svn@3504 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- snapshot-repository/sk/seges/acris/create_release_structure.sh
+++ snapshot-repository/sk/seges/acris/create_release_structure.sh
@@ -5,7 +5,7 @@ if [ ${#@} -ne 1 ] ; then
 	exit 42
 fi
 
-list="acris-os-build acris-os-parent acris-benchmark acris-binding acris-callbacks acris-client-core acris-json acris-common acris-external acris-generator acris-recorder acris-security acris-server-components"
+list="acris-os-build acris-os-parent acris-benchmark acris-binding acris-callbacks acris-client-core acris-common acris-external acris-generator acris-json acris-recorder acris-security-appengine acris-security-core acris-security-gilead acris-security-hibernate acris-security-jpa acris-security-parent acris-security-spring acris-security-spring-hibernate acris-security-spring-jpa acris-server-components acris-widgets acris-showcase-binding acris-showcase-callbacks acris-showcase-client-core acris-showcase-deployer acris-showcase-generator acris-showcase-json acris-showcase-mvp acris-showcase-parent acris-showcase-recorder acris-showcase-security acris-showcase-svn-post-commit acris-showcase-ui-support acris-showcase-widgets"
 
 for project in $list ; do
 	echo "Creating $project/$1"
