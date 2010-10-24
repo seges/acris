6edbfd579f183c17d10ee34a39f5e9960c472807
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2010-10-24 22:04:42 +0000

    Session handler fix, small fixes
    git-svn-id: https://acris.googlecode.com/svn/branches/1.1.0@5035 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-security/acris-security-core/src/main/java/sk/seges/acris/security/server/core/SessionHandlerListener.java
+++ acris-security/acris-security-core/src/main/java/sk/seges/acris/security/server/core/SessionHandlerListener.java
@@ -45,6 +45,9 @@ public class SessionHandlerListener implements HttpSessionListener {
 
 	public static HttpSession getSession(String sessionId) {
 		String mappedSessionId = sessionsMappings.get(sessionId);
+		if (mappedSessionId == null) {
+			return activeSession.get(sessionId);
+		}
 		return activeSession.get(mappedSessionId);
 	}
 
