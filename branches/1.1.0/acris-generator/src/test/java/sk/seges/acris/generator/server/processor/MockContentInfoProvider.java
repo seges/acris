1fe2b47d639c2314f17dd5bf9a0c53e73d6d49fb
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2010-11-04 22:20:31 +0000

    Fixed tests
    git-svn-id: https://acris.googlecode.com/svn@5046 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- branches/1.1.0/acris-generator/src/test/java/sk/seges/acris/generator/server/processor/MockContentInfoProvider.java
+++ branches/1.1.0/acris-generator/src/test/java/sk/seges/acris/generator/server/processor/MockContentInfoProvider.java
@@ -38,6 +38,6 @@ public class MockContentInfoProvider implements IContentInfoProvider {
 
 	@Override
 	public GeneratorToken getDefaultContent(String webId, String lang) {
-		return new GeneratorToken();
+		return null;
 	}
 }
