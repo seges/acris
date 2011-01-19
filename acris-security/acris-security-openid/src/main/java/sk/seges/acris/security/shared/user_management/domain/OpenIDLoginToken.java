be3d282bb6f1432a03425edfb9ec754ee48f65c5
bossobossster <bossobossster@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-01-19 18:16:57 +0000

    added missing files in last commit to acris-security-openid
    git-svn-id: https://acris.googlecode.com/svn/branches/1.1.0@5351 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-security/acris-security-openid/src/main/java/sk/seges/acris/security/shared/user_management/domain/OpenIDLoginToken.java
+++ acris-security/acris-security-openid/src/main/java/sk/seges/acris/security/shared/user_management/domain/OpenIDLoginToken.java
@@ -1,6 +1,7 @@
 package sk.seges.acris.security.shared.user_management.domain;
 
 import sk.seges.acris.security.shared.user_management.domain.api.LoginToken;
+import sk.seges.acris.security.shared.user_management.domain.api.OpenIDProvider;
 
 public class OpenIDLoginToken implements LoginToken {
 
@@ -8,11 +9,22 @@ public class OpenIDLoginToken implements LoginToken {
 
 	private String identifier;
 
+	private String email;
+
+	private OpenIDProvider provider;
+
 	public OpenIDLoginToken() {
+		this(null, null, null);
 	}
 
 	public OpenIDLoginToken(String identifier) {
+		this(identifier, null, null);
+	}
+
+	public OpenIDLoginToken(String identifier, String email, OpenIDProvider provider) {
 		this.identifier = identifier;
+		this.email = email;
+		this.provider = provider;
 	}
 
 	public String getIdentifier() {
@@ -21,5 +33,21 @@ public class OpenIDLoginToken implements LoginToken {
 
 	public void setIdentifier(String identifier) {
 		this.identifier = identifier;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public OpenIDProvider getProvider() {
+		return provider;
+	}
+
+	public void setProvider(OpenIDProvider provider) {
+		this.provider = provider;
 	}
 }
