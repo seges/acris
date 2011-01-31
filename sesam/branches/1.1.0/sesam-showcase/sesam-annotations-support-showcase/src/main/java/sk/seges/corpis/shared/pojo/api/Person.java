4e65155fb7dc019a9e490dca0cf928cfec4ed954
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-01-31 01:10:36 +0000

    Added showcase classes
    git-svn-id: https://acris.googlecode.com/svn@5399 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- sesam/branches/1.1.0/sesam-showcase/sesam-annotations-support-showcase/src/main/java/sk/seges/corpis/shared/pojo/api/Person.java
+++ sesam/branches/1.1.0/sesam-showcase/sesam-annotations-support-showcase/src/main/java/sk/seges/corpis/shared/pojo/api/Person.java
@@ -3,9 +3,9 @@ package sk.seges.corpis.shared.pojo.api;
 import sk.seges.corpis.platform.annotation.TransferObject;
 
 @TransferObject
-public interface PersonData {
-	
-	String getName();
- 
-	void setName(String name);
-} 
+public class Person {
+	String name;  
+	String lastName;  
+	String phone; 
+	String fax;
+}
