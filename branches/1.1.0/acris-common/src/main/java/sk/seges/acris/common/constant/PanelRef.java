c015716778ae45f826501129364e8288a736ed16
bossobossster <bossobossster@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-02-02 22:19:23 +0000

    added fragment flow layout panel reference
    git-svn-id: https://acris.googlecode.com/svn@5419 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- branches/1.1.0/acris-common/src/main/java/sk/seges/acris/common/constant/PanelRef.java
+++ branches/1.1.0/acris-common/src/main/java/sk/seges/acris/common/constant/PanelRef.java
@@ -6,6 +6,7 @@ public interface PanelRef {
 	
     public static final String HTML_LAYOUT_PANEL = "htmlLP";
     public static final String FLOW_LAYOUT_PANEL = "flowLP";
+    public static final String FRAGMENT_FLOW_LAYOUT_PANEL = "fragmentFlowLP";
     public static final String DECK_LAYOUT_PANEL = "deckLP";
     public static final String MENU_LAYOUT_PANEL = "menuLP";
     public static final String SECURED_MENU_LAYOUT_PANEL = "secMenuLP";
