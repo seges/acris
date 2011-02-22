27fe0e0fb142c63541afea2eef4d31fba511a861
bossobossster <bossobossster@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-02-22 12:25:14 +0000

    now query strings are properly read and sent on login and logout
    git-svn-id: https://acris.googlecode.com/svn/branches/1.1.0@5482 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-security/acris-security-core/src/main/java/sk/seges/acris/security/shared/util/LoginConstants.java
+++ acris-security/acris-security-core/src/main/java/sk/seges/acris/security/shared/util/LoginConstants.java
@@ -2,7 +2,10 @@ package sk.seges.acris.security.shared.util;
 
 public class LoginConstants {
 
-	// Session ID string
+	// Login parameters
+	public static final String ACRIS_THEME_STRING = "acristheme";
+	public static final String ACRIS_LOCALE_STRING = "locale";
+	public static final String ACRIS_CODESVR_STRING = "gwt.codesvr";
 	public static final String ACRIS_SESSION_ID_STRING = "sessionid";
 
 	// Login token name
@@ -10,7 +13,7 @@ public class LoginConstants {
 
 	// Logged in user name
 	public static final String LOGGED_USER_NAME = "logged.user.name";
-	
+
 	// Cookies
 	public static final String LANGUAGE_COOKIE_NAME = "acris-languageCookie";
 	public static final String LOGINNAME_COOKIE_NAME = "acris-loginNameCookie";
