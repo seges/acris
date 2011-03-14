a16c9e45d9b5f5f09d1c8ba7f813e23abe870060
bossobossster <bossobossster@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-03-14 15:56:16 +0000

    added realm discovery and more attribute fetching options to openid consumer service
    git-svn-id: https://acris.googlecode.com/svn/branches/1.1.0@5545 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-security/acris-security-openid/src/main/java/sk/seges/acris/security/shared/service/IOpenIDConsumerServiceAsync.java
+++ acris-security/acris-security-openid/src/main/java/sk/seges/acris/security/shared/service/IOpenIDConsumerServiceAsync.java
@@ -2,7 +2,7 @@ package sk.seges.acris.security.shared.service;
 
 import java.util.Map;
 
-import sk.seges.acris.security.shared.data.OpenIDUser;
+import sk.seges.acris.security.shared.dto.OpenIDUserDTO;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
@@ -11,7 +11,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
  */
 public interface IOpenIDConsumerServiceAsync {
 
-	void authenticate(String userSuppliedString, String returnToUrl, AsyncCallback<OpenIDUser> callback);
+	void authenticate(String userSuppliedString, String returnToUrl, String realm, AsyncCallback<OpenIDUserDTO> callback);
 
-	void verify(String queryString, Map<String, String[]> parameterMap, AsyncCallback<String> callback);
+	void verify(String queryString, Map<String, String[]> parameterMap, AsyncCallback<OpenIDUserDTO> callback);
 }
