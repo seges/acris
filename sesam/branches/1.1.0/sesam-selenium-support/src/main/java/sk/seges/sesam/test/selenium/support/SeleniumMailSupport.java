41eee176b0083208978efa2e4f923c943fc8865c
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-05-10 07:57:09 +0000

    Removed not used classes
    git-svn-id: https://acris.googlecode.com/svn@5615 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- sesam/branches/1.1.0/sesam-selenium-support/src/main/java/sk/seges/sesam/test/selenium/support/SeleniumMailSupport.java
+++
@@ -1,9 +0,0 @@
-package sk.seges.sesam.test.selenium.support;
-
-public interface SeleniumMailSupport {
-
-	void waitForMailNotPresent(String subject) throws InterruptedException;
-
-	String waitForMailPresent(String subject) throws InterruptedException;
-
-}
