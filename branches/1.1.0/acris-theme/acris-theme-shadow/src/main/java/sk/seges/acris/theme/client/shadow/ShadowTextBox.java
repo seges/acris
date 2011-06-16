30c9ab27b1e7cdd4a3beebf63283f7e917180f6b
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-06-16 12:22:27 +0000

    Copyright is important :-) So here it is
    git-svn-id: https://acris.googlecode.com/svn@5728 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- branches/1.1.0/acris-theme/acris-theme-shadow/src/main/java/sk/seges/acris/theme/client/shadow/ShadowTextBox.java
+++ branches/1.1.0/acris-theme/acris-theme-shadow/src/main/java/sk/seges/acris/theme/client/shadow/ShadowTextBox.java
@@ -1,8 +1,28 @@
+/**
+ *  Copyright 2011 Seges s.r.o.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package sk.seges.acris.theme.client.shadow;
 
 import sk.seges.acris.theme.client.annotation.ThemeSupport;
 
 import com.google.gwt.user.client.ui.TextBox;
 
+/**
+ * Styled {@link TextBox} with the shadow theme
+ * 
+ * @author Peter Simun (simun@seges.sk)
+ */
 @ThemeSupport(widgetClass = TextBox.class, elementName = "textBox", themeName = ShadowTheme.NAME)
 public interface ShadowTextBox {} 
