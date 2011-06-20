9d988049a9ece3bfbc19f56c454655b44c09a559
simun@seges.sk <simun@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-06-20 13:39:02 +0000

    More processors, more, moooore
    git-svn-id: https://acris.googlecode.com/svn/branches/1.1.0@5765 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ acris-theme/acris-theme-core/src/main/java/sk/seges/acris/theme/pap/ThemeConfigurationProcessor.java
@@ -0,0 +1,112 @@
+package sk.seges.acris.theme.pap;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic.Kind;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
+
+import sk.seges.acris.theme.client.annotation.ThemeConfiguration;
+import sk.seges.acris.theme.client.annotation.ThemeSupport;
+import sk.seges.acris.theme.pap.util.AnnotationClassPropertyHarvester;
+import sk.seges.acris.theme.pap.util.AnnotationClassPropertyHarvester.AnnotationClassProperty;
+import sk.seges.acris.theme.rebind.ThemeComponentSelectorGenerator;
+
+
+@SupportedSourceVersion(SourceVersion.RELEASE_6)
+public class ThemeConfigurationProcessor extends AbstractProcessor {
+
+	@Override
+	public Set<String> getSupportedAnnotationTypes() {
+		Set<String> annotations = new HashSet<String>();
+		annotations.add(ThemeConfiguration.class.getCanonicalName());
+		return annotations;
+	}
+
+	private String toFirstUppercase(String name) {
+		if (name.length() == 1) {
+			return name.toUpperCase();
+		}
+		name = name.toLowerCase();
+		return name.substring(0, 1).toUpperCase() + name.substring(1);
+	}
+	
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+		if (!roundEnv.processingOver()) {
+			Set<? extends Element> configurations = roundEnv.getElementsAnnotatedWith(ThemeConfiguration.class);
+			
+			for (Element configurationElement: configurations) {
+
+				Set<? extends Element> themeComponents = roundEnv.getElementsAnnotatedWith(ThemeSupport.class);
+
+				if (themeComponents.size() == 0) {
+					return false;
+				}
+
+				ThemeConfiguration themeConfiguration = configurationElement.getAnnotation(ThemeConfiguration.class);
+
+				PrintWriter pw = null;
+				
+				try {
+					
+					String packageName = processingEnv.getElementUtils().getPackageOf(configurationElement).getQualifiedName().toString();
+					packageName = packageName.replace(".", "/");
+					
+					FileObject configurationFile = processingEnv.getFiler().createResource(StandardLocation.CLASS_OUTPUT, "", "target/generated-resources/" + 
+							packageName + "/" + toFirstUppercase(themeConfiguration.themeName()) + "ThemeConfiguration.gwt.xml", configurationElement);
+					OutputStream os = configurationFile.openOutputStream();
+					
+					pw = new PrintWriter(os);
+
+					pw.println("<module>");
+					pw.println("	<inherits name='com.google.gwt.user.User' />");
+					pw.println("	<inherits name='sk.seges.acris.Theme' />");
+					pw.println("");
+					pw.println("	<set-property name=\"acristheme\" value=\"" + themeConfiguration.themeName() + "\" />");
+					pw.println("");
+					pw.println("	<generate-with class=\"" + ThemeComponentSelectorGenerator.class.getCanonicalName() + "\">");
+					pw.println("		<any>");
+
+					for (Element themeComponent: themeComponents) {
+						ThemeSupport themeSupport = themeComponent.getAnnotation(ThemeSupport.class);
+
+						TypeElement widgetClass = AnnotationClassPropertyHarvester.getTypeOfClassProperty(themeSupport, new AnnotationClassProperty<ThemeSupport>() {
+
+							@Override
+							public Class<?> getClassProperty(ThemeSupport annotation) {
+								return annotation.widgetClass();
+							}});
+						pw.println("			<when-type-is class=\"" + widgetClass.toString() + "\" />");
+					}
+
+					pw.println("		</any>");
+					pw.println("	</generate-with>");
+					pw.println();
+					pw.println("</module>");
+					
+					pw.flush();
+
+				} catch (IOException e) {
+					e.printStackTrace();
+					processingEnv.getMessager().printMessage(Kind.ERROR, "Unable to process element " + e.getMessage(), configurationElement);
+				} finally {
+					if (pw != null) {
+						pw.close();
+					}
+				}
+			}
+		}
+		return false;
+	}
+}
