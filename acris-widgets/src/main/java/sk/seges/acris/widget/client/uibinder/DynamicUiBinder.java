3600a4b28d3ff51dfcb6fad6485e62c7be5360e2
ladislav.gazo <ladislav.gazo@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-07-17 22:40:55 +0000

    #32 - extracted interface for templateable components + documentation
    git-svn-id: https://acris.googlecode.com/svn/branches/1.1.0@5802 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-widgets/src/main/java/sk/seges/acris/widget/client/uibinder/DynamicUiBinder.java
+++ acris-widgets/src/main/java/sk/seges/acris/widget/client/uibinder/DynamicUiBinder.java
@@ -3,8 +3,10 @@ package sk.seges.acris.widget.client.uibinder;
 import com.google.gwt.uibinder.client.UiBinder;
 
 /**
+ * Represents a panel which components are bound to a dynamic template.
+ * 
  * @author ladislav.gazo
  */
 public interface DynamicUiBinder<U, O> extends UiBinder<U, O> {
-	void setHtml(String html);
+	void setViewTemplate(String viewTemplate);
 }
--- acris-widgets/src/main/java/sk/seges/acris/widget/client/uibinder/DynamicUiPanel.java
+++ acris-widgets/src/main/java/sk/seges/acris/widget/client/uibinder/DynamicUiPanel.java
@@ -1,59 +1,53 @@
-/**
- * Copyright (c) 2011 Seges s.r.o.
- * All Rights Reserved.
- *
- * Any usage, duplication or redistribution of this software is allowed only
- * according to separate agreement prepared in written between Seges
- * and authorized party.
- */
 package sk.seges.acris.widget.client.uibinder;
-
 
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.HTML;
 
 /**
+ * Helper class containing common methods for parsing view template into bound
+ * components.
+ * 
  * @author ladislav.gazo
  */
 public abstract class DynamicUiPanel<U, O> implements DynamicUiBinder<U, O> {
-	protected String html;
-	
-	public void setHtml(String html) {
-		this.html = html;
+	protected String viewTemplate;
+
+	public void setViewTemplate(String viewTemplate) {
+		this.viewTemplate = viewTemplate;
 	}
-	
+
 	protected abstract void assign(Element child, O owner);
-	
+
 	private void parseTemplate(Element root, O owner) {
 		process(root, owner);
 	}
-	
+
 	protected String getFieldName(Element element) {
 		return element.getAttribute("ui:field");
 	}
-	
+
 	private void process(Element root, O owner) {
 		Element child = root.getFirstChildElement();
-		while(child != null) {
+		while (child != null) {
 			assign(child, owner);
-			
-			if(child.hasChildNodes()) {
+
+			if (child.hasChildNodes()) {
 				process(child, owner);
 			}
-			
+
 			child = child.getNextSiblingElement();
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public U createAndBindUi(O owner) {
-		HTML html2 = new HTML(html);
+		HTML html2 = new HTML(viewTemplate);
 		Document.get().getBody().appendChild(html2.getElement());
 		parseTemplate(html2.getElement(), owner);
 		html2.getElement().removeFromParent();
-		return (U)html2;
+		return (U) html2;
 	}
 
 }
