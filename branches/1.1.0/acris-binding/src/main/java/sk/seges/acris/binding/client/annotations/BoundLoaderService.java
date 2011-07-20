6ebc0d795b6c27cbbcec4305593b998300a55fb0
ladislav.gazo <ladislav.gazo@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-07-20 21:47:13 +0000

    doc for the annotation
    git-svn-id: https://acris.googlecode.com/svn@5807 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- branches/1.1.0/acris-binding/src/main/java/sk/seges/acris/binding/client/annotations/BoundLoaderService.java
+++ branches/1.1.0/acris-binding/src/main/java/sk/seges/acris/binding/client/annotations/BoundLoaderService.java
@@ -1,9 +1,15 @@
 package sk.seges.acris.binding.client.annotations;
 
 /**
- * @author eldzi
+ * @author ladislav.gazo
  */
 public @interface BoundLoaderService {
+	/**
+	 * @return GWT RPC service interface responsible for providing entities.
+	 */
 	Class<?> loaderClass() default Void.class;
+	/**
+	 * @return Optional endpoint URL of the service. If left empty, it will not be taken.
+	 */
 	String ref() default "";
 }
