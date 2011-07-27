3755d4aa7dbac30f3e4845876b6c3d95598b0562
nejakyusername <nejakyusername@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-07-27 22:01:26 +0000

    sesam annotations support is now part of the support projects
    git-svn-id: https://acris.googlecode.com/svn/sesam/branches/1.1.0@5844 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- sesam-annotations-support/src/test/java/sk/seges/sesam/core/pap/test/superclass/typed/TestBeanNestedBounds.java
+++
@@ -1,7 +0,0 @@
-package sk.seges.sesam.core.pap.test.superclass.typed;
-
-import java.io.Serializable;
-import java.util.List;
-
-@SuperClassMarker
-public class TestBeanNestedBounds<T extends List<Serializable>> {}
--- sesam-annotations-support/src/test/java/sk/seges/sesam/core/pap/test/superclass/typed/TestBeanUpperBounds.java
+++
@@ -1,6 +0,0 @@
-package sk.seges.sesam.core.pap.test.superclass.typed;
-
-import java.io.Serializable;
-
-@SuperClassMarker
-public class TestBeanUpperBounds<T extends Serializable & Cloneable> {}
