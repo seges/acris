a1a9c158cd7ac2dea0339feef8f965ff62951adc
bossobossster@gmail.com <bossobossster@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-08-03 20:36:15 +0000

    added Content interfaces to site api
    git-svn-id: https://acris.googlecode.com/svn@5873 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- branches/1.1.0/acris-site/acris-site-api/src/main/java/sk/seges/acris/domain/shared/domain/api/ContentData.java
+++ branches/1.1.0/acris-site/acris-site-api/src/main/java/sk/seges/acris/domain/shared/domain/api/ContentData.java
@@ -1,9 +1,16 @@
 package sk.seges.acris.domain.shared.domain.api;
 
+import java.util.Date;
+import java.util.List;
+
 import sk.seges.corpis.shared.domain.api.HasWebId;
 import sk.seges.sesam.domain.IMutableDomainObject;
 
 public interface ContentData<T> extends IMutableDomainObject<T>, HasWebId {
+
+	String getTitle();
+
+	void setTitle(String title);
 
 	String getKeywords();
 
@@ -13,19 +20,89 @@ public interface ContentData<T> extends IMutableDomainObject<T>, HasWebId {
 
 	void setDescription(String description);
 
-	String getTitle();
-
-	void setTitle(String title);
-
 	String getNiceUrl();
 
-	void setNiceUrl(String niceUlr);
+	void setNiceUrl(String niceUrl);
+
+	String getPageName();
+
+	void setPageName(String pageName);
+
+	String getToken();
+
+	void setToken(String token);
 
 	String getContentDetached();
 
-	void setContentDetached(String content);
+	void setContentDetached(String contentDetached);
 
-	boolean isDefaultContent();
+	List<ContentData<T>> getSubContents();
 
-	void setDefaultContent(boolean isDefaultContent);
+	void setSubContents(List<ContentData<T>> subContents);
+
+	void addSubContent(ContentData<T> subContent);
+
+	ContentData<T> getParent();
+
+	void setParent(ContentData<T> parent);
+
+	String getLabel();
+
+	void setLabel(String label);
+
+	Integer getIndex();
+
+	void setIndex(Integer index);
+
+	String getRef();
+
+	void setRef(String ref);
+
+	String getStylePrefix();
+
+	void setStylePrefix(String stylePrefix);
+
+	Date getCreated();
+
+	void setCreated(Date created);
+
+	Date getModified();
+
+	void setModified(Date modified);
+
+	String getPosition();
+
+	void setPosition(String position);
+
+	String getGroup();
+
+	void setGroup(String group);
+
+	Boolean getDefaultlyLoaded();
+
+	void setDefaultlyLoaded(Boolean defaultlyLoaded);
+
+	String getDefaultStyleClass();
+
+	void setDefaultStyleClass(String defaultStyleClass);
+
+	String getMenuItems();
+
+	void setMenuItems(String menuItems);
+
+	String getDecoration();
+
+	void setDecoration(String decoration);
+
+	Boolean getHasChildren();
+
+	void setHasChildren(Boolean hasChildren);
+
+	Integer getVersion();
+
+	void setVersion(Integer version);
+
+	String getParams();
+
+	void setParams(String params);
 }
