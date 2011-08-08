edfcce21391ec45fd6da1cb57c1a5c7c4947946a
ladislav.gazo <ladislav.gazo@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-08-08 21:21:52 +0000

    fixed according to changes in ContentData
    git-svn-id: https://acris.googlecode.com/svn@5887 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- branches/1.1.0/acris-showcase/acris-showcase-generator/src/main/java/sk/seges/acris/generator/server/processor/MoviesContent.java
+++ branches/1.1.0/acris-showcase/acris-showcase-generator/src/main/java/sk/seges/acris/generator/server/processor/MoviesContent.java
@@ -1,5 +1,8 @@
 package sk.seges.acris.generator.server.processor;
 
+import java.util.Date;
+import java.util.List;
+
 import sk.seges.acris.domain.shared.domain.api.ContentData;
 import sk.seges.acris.showcase.mora.client.configuration.NameTokens;
 
@@ -72,11 +75,236 @@ public class MoviesContent implements ContentData<Long> {
 	}
 
 	@Override
-	public boolean isDefaultContent() {
-		return true;
+	public String getPageName() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
-	public void setDefaultContent(boolean isDefaultContent) {
+	public void setPageName(String pageName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getToken() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setToken(String token) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ContentData<Long>> getSubContents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSubContents(List<ContentData<Long>> subContents) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addSubContent(ContentData<Long> subContent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ContentData<Long> getParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setParent(ContentData<Long> parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getLabel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setLabel(String label) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Integer getIndex() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setIndex(Integer index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getRef() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRef(String ref) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getStylePrefix() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStylePrefix(String stylePrefix) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Date getCreated() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCreated(Date created) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Date getModified() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setModified(Date modified) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPosition(String position) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getGroup() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setGroup(String group) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Boolean getDefaultlyLoaded() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDefaultlyLoaded(Boolean defaultlyLoaded) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getDefaultStyleClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDefaultStyleClass(String defaultStyleClass) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getMenuItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setMenuItems(String menuItems) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getDecoration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDecoration(String decoration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Boolean getHasChildren() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setHasChildren(Boolean hasChildren) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Integer getVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setVersion(Integer version) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getParams() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setParams(String params) {
+		// TODO Auto-generated method stub
+		
 	}
 }
