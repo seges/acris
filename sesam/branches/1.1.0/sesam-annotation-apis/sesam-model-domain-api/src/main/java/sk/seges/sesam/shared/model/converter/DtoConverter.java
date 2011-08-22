02523ddcdc74bdc702f0d027c250d5232e9a7cc9
bossobossster@gmail.com <bossobossster@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-08-22 14:00:08 +0000

    fixed wrong collection class in dto converter
    git-svn-id: https://acris.googlecode.com/svn@5922 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- sesam/branches/1.1.0/sesam-annotation-apis/sesam-model-domain-api/src/main/java/sk/seges/sesam/shared/model/converter/DtoConverter.java
+++ sesam/branches/1.1.0/sesam-annotation-apis/sesam-model-domain-api/src/main/java/sk/seges/sesam/shared/model/converter/DtoConverter.java
@@ -9,10 +9,12 @@ public interface DtoConverter<DTO, DOMAIN> {
 	DTO convertToDto(DTO result, DOMAIN domain);
 	DTO toDto(DOMAIN domain);
 	Collection<DTO> toDto(Collection<?> domains);
+	<T extends Collection<DTO>> T toDto(Collection<?> domains, Class<T> targetClass);
 	Map<?, ?> toDto(Map<?, ?> domains);
 	
 	DOMAIN convertFromDto(DOMAIN result, DTO domain);
 	DOMAIN fromDto(DTO dto);
 	Collection<DOMAIN> fromDto(Collection<?> dtos);
+	<T extends Collection<DOMAIN>> T fromDto(Collection<?> dtos, Class<T> targetClass);
 	Map<?, ?> fromDto(Map<?, ?> dtos);
 }
