2d675f1a17a2aa6cfbd8b2d662f7684a423b90ba
bossobossster@gmail.com <bossobossster@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-08-24 17:37:16 +0000

    type fixes in dto converter
    git-svn-id: https://acris.googlecode.com/svn@5926 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- sesam/branches/1.1.0/sesam-annotation-apis/sesam-model-domain-api/src/main/java/sk/seges/sesam/shared/model/converter/ConvertedInstanceCache.java
+++ sesam/branches/1.1.0/sesam-annotation-apis/sesam-model-domain-api/src/main/java/sk/seges/sesam/shared/model/converter/ConvertedInstanceCache.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 
 public interface ConvertedInstanceCache {
 	
+	<S> S getInstance(Object source);
+	<S> S putInstance(Object source, S result);
+
 	<S> S getInstance(Class<S> instanceClass, Serializable id);
-	
 	<S> S putInstance(S instance, Serializable id);
 }
--- sesam/branches/1.1.0/sesam-annotation-apis/sesam-model-domain-api/src/main/java/sk/seges/sesam/shared/model/converter/MapConvertedInstanceCache.java
+++ sesam/branches/1.1.0/sesam-annotation-apis/sesam-model-domain-api/src/main/java/sk/seges/sesam/shared/model/converter/MapConvertedInstanceCache.java
@@ -6,12 +6,23 @@ import java.util.Map;
 
 public class MapConvertedInstanceCache implements ConvertedInstanceCache {
 
-	private Map<Class<?>, Map<Serializable, Object>> instanceCache = new HashMap<Class<?>, Map<Serializable, Object>>();
+	private Map<Class<?>, Map<Serializable, Object>> instanceIdCache = new HashMap<Class<?>, Map<Serializable, Object>>();
+	private Map<Class<?>, Map<Object, Object>> instancesCache = new HashMap<Class<?>, Map<Object, Object>>();
+
+	@SuppressWarnings("unchecked")
+	public <S> S getInstance(Object source) {
+		Map<Object, Object> map = instancesCache.get(source.getClass());
+
+		if (map == null) {
+			return null;
+		}
+		return (S) map.get(source);
+	}
 	
 	@SuppressWarnings("unchecked")
 	@Override
 	public <S> S getInstance(Class<S> instanceClass, Serializable id) {
-		Map<Serializable, Object> map = instanceCache.get(instanceClass);
+		Map<Serializable, Object> map = instanceIdCache.get(instanceClass);
 		if (map == null) {
 			return null;
 		}
@@ -19,16 +30,30 @@ public class MapConvertedInstanceCache implements ConvertedInstanceCache {
 	}
 
 	@Override
-	public <S> S putInstance(S instance, Serializable id) {
-		Map<Serializable, Object> instances = (Map<Serializable, Object>) instanceCache.get(instance.getClass());
+	public <S> S putInstance(Object source, S result) {
+		Map<Object, Object> instances = (Map<Object, Object>) instancesCache.get(source.getClass());
+
+		if (instances == null) {
+			instances = new HashMap<Object, Object>();
+			instancesCache.put(source.getClass(), instances);
+		}
+
+		instances.put(source, result);
+		
+		return result;
+	}
+	
+	@Override
+	public <S> S putInstance(S result, Serializable id) {
+		Map<Serializable, Object> instances = (Map<Serializable, Object>) instanceIdCache.get(result.getClass());
 		
 		if (instances == null) {
 			instances = new HashMap<Serializable, Object>();
-			instanceCache.put(instance.getClass(), instances);
+			instanceIdCache.put(result.getClass(), instances);
 		}
 		
-		instances.put(id, instance);
+		instances.put(id, result);
 		
-		return instance;
+		return result;
 	}
 }
