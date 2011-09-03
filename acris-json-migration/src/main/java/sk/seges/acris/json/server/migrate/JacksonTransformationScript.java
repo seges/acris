d1e2f166c3fc029b55511e419a5a25616b53e55a
ladislav.gazo <ladislav.gazo@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-09-03 15:01:53 +0000

    #34 - script helper methods added to ease writing
    git-svn-id: https://acris.googlecode.com/svn/branches/1.1.0@5949 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-json-migration/src/main/java/sk/seges/acris/json/server/migrate/JacksonTransformationScript.java
+++ acris-json-migration/src/main/java/sk/seges/acris/json/server/migrate/JacksonTransformationScript.java
@@ -1,12 +1,21 @@
 package sk.seges.acris.json.server.migrate;
 
+import java.util.Arrays;
+
 import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.BooleanNode;
 import org.codehaus.jackson.node.ObjectNode;
+import org.codehaus.jackson.node.TextNode;
 
 /**
+ * Template class for all Jackson-based transformation scripts. Contains various
+ * helper methods to ease writing scripts.
+ * 
  * @author ladislav.gazo
- *
- * @param <T> Root node type
+ * 
+ * @param <T>
+ *            Root node type
  */
 public abstract class JacksonTransformationScript<T extends JsonNode> {
 	public void execute(T node) {
@@ -14,10 +23,68 @@ public abstract class JacksonTransformationScript<T extends JsonNode> {
 	}
 
 	protected abstract void process(T node);
-	
+
+	/**
+	 * Renames field in an object node.
+	 * 
+	 * @param parent
+	 *            Object node where to rename the field.
+	 * @param srcField
+	 *            Original field name.
+	 * @param dstField
+	 *            Desired field name.
+	 */
 	protected void rename(ObjectNode parent, String srcField, String dstField) {
 		JsonNode value = parent.path(srcField);
 		parent.put(dstField, value);
 		parent.remove(srcField);
+	}
+
+	/**
+	 * Adds child node to a parent in case it does not exist already. New child
+	 * is taken as a childPrototype parameter. In case it exists, nothing
+	 * happens.
+	 * 
+	 * @param <N>
+	 *            Type of the child node.
+	 * @param parent
+	 *            Parent node where to look for the field.
+	 * @param field
+	 *            Field name in object node.
+	 * @param childPrototype
+	 *            Prepared instance of a child node in case it doesn't exist.
+	 * @return If the field exists in the parent, the node for the field is
+	 *         returned, otherwise childPrototype is returned.
+	 */
+	@SuppressWarnings("unchecked")
+	protected <N extends JsonNode> N addNonExistent(ObjectNode parent, String field, N childPrototype) {
+		JsonNode child = parent.get(field);
+		if (child != null && !child.isObject()) {
+			throw new RuntimeException("Field " + field + " exists and is not an object, cannot continue");
+		} else if (child != null) {
+			return (N) child;
+		} else if (child == null) {
+			parent.put(field, childPrototype);
+		}
+
+		return childPrototype;
+	}
+	
+	protected ObjectNode addNonExistentObjectNode(ObjectNode parent, String field) {
+		return addNonExistent(parent, field, parent.objectNode());
+	}
+	
+	protected TextNode addNonExistentObjectNode(ObjectNode parent, String field, String text) {
+		return addNonExistent(parent, field, parent.textNode(text));
+	}
+	
+	protected BooleanNode addNonExistentBooleanNode(ObjectNode parent, String field, boolean b) {
+		return addNonExistent(parent, field, parent.booleanNode(b));
+	}
+	
+	protected ArrayNode addNonExistentArrayNode(ObjectNode parent, String field, JsonNode[] items) {
+		ArrayNode arrayNode = parent.arrayNode();
+		arrayNode.addAll(Arrays.asList(items));
+		return addNonExistent(parent, field, arrayNode);
 	}
 }
