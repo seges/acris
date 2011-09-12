098c6d62c8f2f00ab389445f03d8a089e74b15a9
ladislav.gazo <ladislav.gazo@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-09-12 18:45:21 +0000

    migration concept
    git-svn-id: https://acris.googlecode.com/svn@6015 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ branches/1.1.0/acris-showcase/acris-showcase-appconstructor/src/main/java/sk/seges/acris/scaffold/migration/domain/versioning/CustomerModelToCustomerModelVersion1Migration.java
@@ -0,0 +1,11 @@
+package sk.seges.acris.scaffold.migration.domain.versioning;
+
+/**
+ * @author ladislav.gazo
+ */
+public class CustomerModelToCustomerModelVersion1Migration {
+	public void migrate() {
+		// whatever needed to switch from secondName to surname ... it can be
+		// Liquibase XML or Java or acris-json-migration script
+	}
+}
