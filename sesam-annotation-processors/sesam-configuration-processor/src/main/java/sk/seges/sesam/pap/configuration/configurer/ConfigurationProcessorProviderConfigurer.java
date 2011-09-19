810ffac4aee930ffb48522a0c88a8d2633e598a9
nejakyusername@gmail.com <nejakyusername@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-09-19 09:34:27 +0000

    Configuration file renamed
    git-svn-id: https://acris.googlecode.com/svn/sesam/branches/1.1.0@6036 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ sesam-annotation-processors/sesam-configuration-processor/src/main/java/sk/seges/sesam/pap/configuration/configurer/ConfigurationProcessorProviderConfigurer.java
@@ -0,0 +1,19 @@
+package sk.seges.sesam.pap.configuration.configurer;
+
+import java.lang.reflect.Type;
+
+import sk.seges.sesam.core.configuration.annotation.SettingsProvider;
+import sk.seges.sesam.core.pap.configuration.DefaultProcessorConfigurer;
+
+public class ConfigurationProcessorProviderConfigurer extends DefaultProcessorConfigurer {
+
+	@Override
+	protected Type[] getConfigurationElement(DefaultConfigurationElement element) {
+		switch (element) {
+		case PROCESSING_ANNOTATIONS:
+			return new Type[] { SettingsProvider.class };
+		}
+		return super.getConfigurationElement(element);
+	}
+
+}
