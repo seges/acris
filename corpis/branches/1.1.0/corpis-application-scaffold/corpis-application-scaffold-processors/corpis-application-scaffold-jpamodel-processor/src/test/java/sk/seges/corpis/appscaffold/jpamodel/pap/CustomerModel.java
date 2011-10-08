f752599199c881058be0c7db449de098c27fac67
ladislav.gazo <ladislav.gazo@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-10-08 15:43:57 +0000

    using meta model to showcase propagation of e.g. @UniqueConstraints to the resulting generated JPA class
    git-svn-id: https://acris.googlecode.com/svn@6158 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- corpis/branches/1.1.0/corpis-application-scaffold/corpis-application-scaffold-processors/corpis-application-scaffold-jpamodel-processor/src/test/java/sk/seges/corpis/appscaffold/jpamodel/pap/CustomerModel.java
+++ corpis/branches/1.1.0/corpis-application-scaffold/corpis-application-scaffold-processors/corpis-application-scaffold-jpamodel-processor/src/test/java/sk/seges/corpis/appscaffold/jpamodel/pap/CustomerModel.java
@@ -1,7 +1,9 @@
 package sk.seges.corpis.appscaffold.jpamodel.pap;
 
 import sk.seges.corpis.appscaffold.shared.annotation.DomainInterface;
+import sk.seges.sesam.model.metadata.annotation.MetaModel;
 
+@MetaModel
 @DomainInterface
 public interface CustomerModel<T> {
 	T id();
--- corpis/branches/1.1.0/corpis-application-scaffold/corpis-application-scaffold-processors/corpis-application-scaffold-jpamodel-processor/src/test/java/sk/seges/corpis/appscaffold/jpamodel/pap/JpaThemeModel.java
+++ corpis/branches/1.1.0/corpis-application-scaffold/corpis-application-scaffold-processors/corpis-application-scaffold-jpamodel-processor/src/test/java/sk/seges/corpis/appscaffold/jpamodel/pap/JpaThemeModel.java
@@ -3,6 +3,8 @@ package sk.seges.corpis.appscaffold.jpamodel.pap;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import sk.seges.corpis.appscaffold.shared.annotation.domain.BusinessKey;
 import sk.seges.corpis.appscaffold.shared.annotation.domain.JpaModel;
@@ -11,8 +13,8 @@ import sk.seges.corpis.appscaffold.shared.annotation.domain.JpaModel;
  * @author ladislav.gazo
  */
 @JpaModel
-//@Table(name = "theme", uniqueConstraints = { @UniqueConstraint(columnNames = {
-//		ThemeMetaModel.WEB_ID, ThemeMetaModel.NAME }) })
+@Table(name = "theme", uniqueConstraints = { @UniqueConstraint(columnNames = {
+		ThemeModelMetaModel.WEB_ID, ThemeModelMetaModel.NAME }) })
 @SequenceGenerator(name = "seqThemes", sequenceName = "SEQ_THEMES", initialValue = 1)
 public interface JpaThemeModel extends ThemeModel<Long> {
 	@Id
