949cc36d1571567ef484067e7d759e426736eb64
ladislav.gazo <ladislav.gazo@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-10-09 21:56:00 +0000

    #48 - added support for extended remote service interfaces
    git-svn-id: https://acris.googlecode.com/svn/trunk@6227 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-annotation-processors/acris-async-service-processor/src/main/java/sk/seges/acris/pap/service/model/AsyncRemoteServiceType.java
+++ acris-annotation-processors/acris-async-service-processor/src/main/java/sk/seges/acris/pap/service/model/AsyncRemoteServiceType.java
@@ -1,6 +1,7 @@
 package sk.seges.acris.pap.service.model;
 
 import sk.seges.sesam.core.pap.model.mutable.api.MutableDeclaredType;
+import sk.seges.sesam.core.pap.model.mutable.api.MutableTypeVariable;
 import sk.seges.sesam.core.pap.model.mutable.delegate.DelegateMutableDeclaredType;
 import sk.seges.sesam.pap.service.model.RemoteServiceTypeElement;
 
@@ -9,16 +10,21 @@ public class AsyncRemoteServiceType extends DelegateMutableDeclaredType {
 	public static final String ASYNC_SUFFIX = "Async";
 
 	private RemoteServiceTypeElement remoteService;
-	
+
 	public AsyncRemoteServiceType(RemoteServiceTypeElement remoteService) {
 		this.remoteService = remoteService;
-		
-		setKind(MutableTypeKind.INTERFACE);	
+
+		setKind(MutableTypeKind.INTERFACE);
 	}
-	
+
 	@Override
 	protected MutableDeclaredType getDelegate() {
-		return remoteService.clone().addClassSufix(ASYNC_SUFFIX);
+		return remoteService
+				.clone()
+				.addClassSufix(ASYNC_SUFFIX)
+				.setTypeVariables(
+						remoteService.getTypeVariables().toArray(
+								new MutableTypeVariable[] {}));
 	}
 
 }
