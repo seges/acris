87e7d1904819825fbac63737295b95d58ac9d72c
nejakyusername <nejakyusername@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-10-17 12:50:40 +0000

    #52 - contentDTO is now generated from content data (to be more precise, from ContentBlobBase)
    git-svn-id: https://acris.googlecode.com/svn/branches/dto_rework@6301 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ acris-site/acris-site-dto/src/main/java/sk/seges/acris/server/model/dto/configuration/ContentPkDataDTOConfiguration.java
@@ -0,0 +1,8 @@
+package sk.seges.acris.server.model.dto.configuration;
+
+import sk.seges.acris.domain.shared.domain.api.ContentPkData;
+import sk.seges.sesam.pap.model.annotation.TransferObjectMapping;
+
+@TransferObjectMapping(domainClass = ContentPkData.class, configuration = ContentPkDTOConfiguration.class)
+public interface ContentPkDataDTOConfiguration {
+}
