2300443179aeb5e078b991124a95ec3bdc9eb40d
bossobossster@gmail.com <bossobossster@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-10-26 14:43:51 +0000

    fixed equals and hashcode for contents, annotations are now copied to dto
    git-svn-id: https://acris.googlecode.com/svn/trunk@6331 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-site/acris-site-dto/src/main/java/sk/seges/acris/server/model/dto/configuration/ContentPkDTOConfiguration.java
+++ acris-site/acris-site-dto/src/main/java/sk/seges/acris/server/model/dto/configuration/ContentPkDTOConfiguration.java
@@ -2,11 +2,15 @@ package sk.seges.acris.server.model.dto.configuration;
 
 import sk.seges.acris.core.client.rpc.IDataTransferObject;
 import sk.seges.acris.domain.shared.domain.base.ContentPkBase;
+import sk.seges.sesam.pap.model.annotation.GenerateEquals;
+import sk.seges.sesam.pap.model.annotation.GenerateHashcode;
 import sk.seges.sesam.pap.model.annotation.Mapping;
 import sk.seges.sesam.pap.model.annotation.Mapping.MappingType;
 import sk.seges.sesam.pap.model.annotation.TransferObjectMapping;
 
 @TransferObjectMapping(domainClass = ContentPkBase.class)
 @Mapping(MappingType.AUTOMATIC)
+@GenerateEquals(generate = false)
+@GenerateHashcode(generate = false)
 public interface ContentPkDTOConfiguration extends IDataTransferObject {
 }
