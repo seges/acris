dfc53df5280b8a933722ca72fdb41ed83c472a2d
nejakyusername <nejakyusername@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-10-30 19:58:55 +0000

    #39 fix for multiple property converters
    git-svn-id: https://acris.googlecode.com/svn@6339 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- sesam/trunk/sesam-annotation-processors/sesam-model-metadata/src/test/java/sk/seges/sesam/shared/model/mock/MultipleConverterEntity.java
+++ sesam/trunk/sesam-annotation-processors/sesam-model-metadata/src/test/java/sk/seges/sesam/shared/model/mock/MultipleConverterEntity.java
@@ -6,13 +6,23 @@ import sk.seges.sesam.model.metadata.strategy.PojoPropertyConverter;
 
 @MetaModel(beanPropertyConverter = {PojoPropertyConverter.class, DBPropertyConverter.class})
 public class MultipleConverterEntity {
+
 	private String name;
-	
+	private MockHierarchyEntityData parent;
+
 	public String getName() {
 		return name;
 	}
 	
 	public void setName(String name) {
 		this.name = name;
+	}
+		
+	public MockHierarchyEntityData getParent() {
+		return parent;
+	}
+
+	public void setParent(MockHierarchyEntityData entity) {
+		this.parent = entity;
 	}
 }
