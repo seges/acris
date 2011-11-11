f1b43fae9839bedd0548836afeb3d2ec01f9eeb9
bossobossster <bossobossster@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-11-11 10:15:02 +0000

    updated login services
    git-svn-id: https://acris.googlecode.com/svn/trunk@6378 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-security/acris-security-openid/src/main/java/sk/seges/acris/security/server/user_management/service/spring/SpringOpenIDLoginService.java
+++ acris-security/acris-security-openid/src/main/java/sk/seges/acris/security/server/user_management/service/spring/SpringOpenIDLoginService.java
@@ -2,7 +2,7 @@ package sk.seges.acris.security.server.user_management.service.spring;
 
 import org.springframework.transaction.annotation.Transactional;
 
-import sk.seges.acris.security.server.user_management.dao.IGrantsDao;
+import sk.seges.acris.security.server.core.user_management.dao.user.IGenericUserDao;
 import sk.seges.acris.security.server.user_management.dao.api.IOpenIDUserDao;
 import sk.seges.acris.security.server.user_management.service.OpenIDLoginService;
 import sk.seges.acris.security.server.utils.TokenConverter;
@@ -11,12 +11,13 @@ import sk.seges.acris.security.shared.session.ClientSession;
 import sk.seges.acris.security.shared.session.SessionIDGenerator;
 import sk.seges.acris.security.shared.user_management.domain.api.HasOpenIDIdentifier;
 import sk.seges.acris.security.shared.user_management.domain.api.LoginToken;
+import sk.seges.acris.security.shared.user_management.domain.api.UserData;
 
 public class SpringOpenIDLoginService extends OpenIDLoginService {
 
-	public SpringOpenIDLoginService(TokenConverter tokenConverter, IGrantsDao grantsDao,
+	public SpringOpenIDLoginService(TokenConverter tokenConverter, IGenericUserDao<UserData<?>> userDao,
 			IOpenIDUserDao<HasOpenIDIdentifier> openIDUserDao, SessionIDGenerator sessionIDGenerator) {
-		super(tokenConverter, grantsDao, openIDUserDao, sessionIDGenerator);
+		super(tokenConverter, userDao, openIDUserDao, sessionIDGenerator);
 	}
 
 	@Transactional
