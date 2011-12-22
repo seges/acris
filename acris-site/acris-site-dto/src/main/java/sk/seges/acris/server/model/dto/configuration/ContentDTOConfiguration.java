8fc2b68a0a73495f4422c23ecc885cae951856dc
sloboda@seges.sk <sloboda@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-12-22 10:38:57 +0000

    Remove not needed import
    git-svn-id: https://acris.googlecode.com/svn/trunk@6451 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-site/acris-site-dto/src/main/java/sk/seges/acris/server/model/dto/configuration/ContentDTOConfiguration.java
+++ acris-site/acris-site-dto/src/main/java/sk/seges/acris/server/model/dto/configuration/ContentDTOConfiguration.java
@@ -2,7 +2,6 @@ package sk.seges.acris.server.model.dto.configuration;
 
 import java.util.Date;
 
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
