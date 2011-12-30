d8e04cf406c69d28377e04b6a5354c5906fd2e92
ladislav.gazo <ladislav.gazo@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2011-12-30 16:23:20 +0000

    double-checking whether compilation works in eclipse + logging
    git-svn-id: https://acris.googlecode.com/svn@6467 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- sesam/trunk/sesam-selenium-support/src/test/java/sk/seges/sesam/pap/test/selenium/processor/AdvancedSeleniumTestProcessorTest.java
+++ sesam/trunk/sesam-selenium-support/src/test/java/sk/seges/sesam/pap/test/selenium/processor/AdvancedSeleniumTestProcessorTest.java
@@ -20,6 +20,12 @@ public class AdvancedSeleniumTestProcessorTest extends AnnotationTest {
 		assertOutput(getResourceFile(AdvancedMockSelenise.class), getOutputFile(AdvancedMockSelenise.class));
 	}
 
+	@Test
+	public void testTestConfigurationForEclipse() {
+		assertCompilationSuccessful(compileFiles(Compiler.ECLIPSE, AdvancedMockSelenise.class, MockSuite.class));
+		assertOutput(getResourceFile(AdvancedMockSelenise.class), getOutputFile(AdvancedMockSelenise.class));
+	}
+
 	private File getOutputFile(Class<?> clazz) {
 		final MutableDeclaredType outputClass = toMutable(clazz).addClassSufix(SeleniumTestSettingsType.SUFFIX);
 		return new File(OUTPUT_DIRECTORY, toPath(outputClass.getPackageName()) + "/" + outputClass.getSimpleName() + SOURCE_FILE_SUFFIX);
