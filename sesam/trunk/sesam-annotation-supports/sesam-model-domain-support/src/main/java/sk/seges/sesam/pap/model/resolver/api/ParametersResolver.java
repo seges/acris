8eecfeb0ac3d9d26534499ec2193326d8fa2287b
nejakyusername <nejakyusername@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-01-16 20:57:56 +0000

    #79 few more DTO fixes for eclipse added copyright to some classes
    git-svn-id: https://acris.googlecode.com/svn@6485 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- sesam/trunk/sesam-annotation-supports/sesam-model-domain-support/src/main/java/sk/seges/sesam/pap/model/resolver/api/ParametersResolver.java
+++ sesam/trunk/sesam-annotation-supports/sesam-model-domain-support/src/main/java/sk/seges/sesam/pap/model/resolver/api/ParametersResolver.java
@@ -3,7 +3,6 @@ package sk.seges.sesam.pap.model.resolver.api;
 import javax.lang.model.type.TypeMirror;
 
 import sk.seges.sesam.core.pap.model.ParameterElement;
-import sk.seges.sesam.pap.model.model.ConverterTypeElement;
 
 public interface ParametersResolver {
 
