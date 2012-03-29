05bb91d1d87ff00e07b7f3086bb1be32e0c50e00
sloboda@seges.sk <sloboda@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-03-29 07:45:32 +0000

    #4075 - Improve demo apiKey authentication
    git-svn-id: https://acris.googlecode.com/svn/trunk@6608 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-security/acris-security-rest-authentication/src/main/java/sk/seges/acris/security/rest/RestAuthenticationResource.java
+++ acris-security/acris-security-rest-authentication/src/main/java/sk/seges/acris/security/rest/RestAuthenticationResource.java
@@ -2,27 +2,35 @@ package sk.seges.acris.security.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import sk.seges.acris.security.server.core.user_management.context.APIKeyUserService;
+import sk.seges.acris.crypto.util.Hasher;
 
 @Path("/auth")
 public class RestAuthenticationResource {
 	
+	private static final Logger log = LoggerFactory.getLogger(RestAuthenticationResource.class);
+
 	@GET
-	@Path("/user?" + APIKeyUserService.WEBID_PARAMETER + "={webId}&" + APIKeyUserService.APIKEY_PARAMETER + "={apiKey}")
+	@Path("/user")
 	@Produces({ MediaType.APPLICATION_JSON })
-	public JSONObject authenticateUser(@PathParam("webId") String webId, @PathParam("apiKey") String apiKey) throws JSONException {
-		JSONObject result;
-		if (webId != null && webId.matches("demo*.\\.synapso\\.sk")) {
-			result = new JSONObject().put("allowed", true);
-		} else {
-			result = new JSONObject().put("allowed", false);
+	public JSONObject authenticate(@QueryParam("webId") String webId, @QueryParam("apiKey") String apiKey) {
+		JSONObject result = null;
+		try {
+			if (webId != null && (Hasher.getSHAHexDigest(webId).equals(apiKey))) {
+				result = new JSONObject().put("allowed", true);
+			} else {
+				result = new JSONObject().put("allowed", false);
+			}
+		} catch (JSONException e) {
+			log.error("Could not parse json, cause:" , e);
 		}
 		return result;
 	}
