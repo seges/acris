57049fd0ea308e9d91cb5faf5a048da48a0dfd5b
bossobossster@gmail.com <bossobossster@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-04-10 11:37:34 +0000

    removed myopenid from login
    git-svn-id: https://acris.googlecode.com/svn@6626 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- trunk/acris-security/acris-security-core/src/main/java/sk/seges/acris/security/shared/util/LoginConstants.java
+++ trunk/acris-security/acris-security-core/src/main/java/sk/seges/acris/security/shared/util/LoginConstants.java
@@ -27,7 +27,7 @@ public class LoginConstants {
 	public static final String GOOGLE_IDENTIFIER = "https://www.google.com/accounts/o8/id";
 	public static final String YAHOO_IDENTIFIER = "http://me.yahoo.com";
 	public static final String AOL_IDENTIFIER = "https://www.aol.com";
-	public static final String BLOGGER_IDENTIFIER = "http://username.wordpress.com";
 	public static final String SEZNAM_IDENTIFIER = "http://www.seznam.cz";
-	public static final String MYOPENID_IDENTIFIER = "https://www.myopenid.com";
+//	public static final String BLOGGER_IDENTIFIER = "http://username.wordpress.com";
+//	public static final String MYOPENID_IDENTIFIER = "https://www.myopenid.com";
 }
