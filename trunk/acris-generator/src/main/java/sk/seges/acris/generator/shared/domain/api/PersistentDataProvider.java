5c0f6e40d83dfcee807c73bd0891ed1be7ab37b5
psenicka1301@gmail.com <psenicka1301@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-05-23 15:44:08 +0000

    #4321: vyhodit corpis-domain-api z projektov
    git-svn-id: https://acris.googlecode.com/svn@6742 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- trunk/acris-generator/src/main/java/sk/seges/acris/generator/shared/domain/api/PersistentDataProvider.java
+++ trunk/acris-generator/src/main/java/sk/seges/acris/generator/shared/domain/api/PersistentDataProvider.java
@@ -1,6 +1,6 @@
 package sk.seges.acris.generator.shared.domain.api;
 
-import sk.seges.corpis.shared.domain.api.HasWebId;
+import sk.seges.corpis.server.domain.HasWebId;
 import sk.seges.sesam.domain.IMutableDomainObject;
 
 public interface PersistentDataProvider extends IMutableDomainObject<String>, HasWebId {
