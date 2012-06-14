fa240c0e9e14b027c765710fa3b11e22e00377fc
sivak@seges.sk <sivak@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-06-14 12:52:24 +0000

    security fix - html escape in column grid
    git-svn-id: https://acris.googlecode.com/svn@6826 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- trunk/acris-widgets-beantable/src/main/java/sk/seges/acris/widget/client/table/CustomFixedWidthGrid.java
+++ trunk/acris-widgets-beantable/src/main/java/sk/seges/acris/widget/client/table/CustomFixedWidthGrid.java
@@ -8,13 +8,16 @@ import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.event.dom.client.HasDoubleClickHandlers;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.gen2.table.client.FixedWidthGrid;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Event;
 
 /**
- * extended FixedWidthGrid, which is used in BeanTable, necessary for double-click handler
+ * extended FixedWidthGrid, which is used in BeanTable, necessary for
+ * double-click handler
+ * 
  * @author marta
- *
+ * 
  */
 
 @SuppressWarnings("unchecked")
@@ -36,14 +39,12 @@ public class CustomFixedWidthGrid extends FixedWidthGrid implements HasDoubleCli
 	public HandlerRegistration addDoubleClickHandler(DoubleClickHandler handler) {
 		return super.addDomHandler(handler, DoubleClickEvent.getType());
 	}
-	
 
 	@Override
 	public HandlerRegistration addClickHandler(ClickHandler handler) {
 		return super.addDomHandler(handler, ClickEvent.getType());
 	}
-	
-	
+
 	@Override
 	public void onBrowserEvent(Event event) {
 		super.onBrowserEvent(event);
@@ -55,5 +56,11 @@ public class CustomFixedWidthGrid extends FixedWidthGrid implements HasDoubleCli
 			ClickEvent.fireNativeEvent(event, this);
 			break;
 		}
+	}
+
+	@Override
+	public void setHTML(int row, int column, String html) {
+		super.setHTML(row, column, SafeHtmlUtils.htmlEscape(html));
+		clearIdealWidths();
 	}
 }
