064021ad148b0379fa50a696fb4cf3709d05a4d4
nejakyusername <nejakyusername@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-06-27 10:20:44 +0000

    Product is now mutable domain object
    git-svn-id: https://acris.googlecode.com/svn@6850 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- corpis/trunk/corpis-domain/corpis-invoice-domain-model/src/main/java/sk/seges/corpis/server/domain/invoice/Product.java
+++ corpis/trunk/corpis-domain/corpis-invoice-domain-model/src/main/java/sk/seges/corpis/server/domain/invoice/Product.java
@@ -7,11 +7,11 @@ import sk.seges.corpis.appscaffold.shared.annotation.BaseObject;
 import sk.seges.corpis.appscaffold.shared.annotation.DomainInterface;
 import sk.seges.corpis.server.domain.Name;
 import sk.seges.corpis.server.domain.customer.Customer;
-import sk.seges.sesam.domain.IDomainObject;
+import sk.seges.sesam.domain.IMutableDomainObject;
 
 @DomainInterface
 @BaseObject
-public interface Product extends IDomainObject<Long> {
+public interface Product extends IMutableDomainObject<Long> {
 
 	String extId();
 
