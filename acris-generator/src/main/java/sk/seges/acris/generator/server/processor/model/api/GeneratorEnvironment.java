1983bcfcd22390fae01a34c84448fc3c3dfc1ba6
nejakyusername <nejakyusername@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-06-29 21:41:35 +0000

    organize imports
    git-svn-id: https://acris.googlecode.com/svn/trunk@6854 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-generator/src/main/java/sk/seges/acris/generator/server/processor/model/api/GeneratorEnvironment.java
+++ acris-generator/src/main/java/sk/seges/acris/generator/server/processor/model/api/GeneratorEnvironment.java
@@ -2,7 +2,6 @@ package sk.seges.acris.generator.server.processor.model.api;
 
 import sk.seges.acris.domain.shared.domain.api.ContentData;
 import sk.seges.acris.generator.shared.domain.GeneratorToken;
-import sk.seges.acris.site.shared.domain.api.WebSettings;
 import sk.seges.acris.site.shared.domain.api.server.model.data.WebSettingsData;
 
 public interface GeneratorEnvironment {
