638665768e9932b5178c1fac979910d90005d95f
nejakyusername@gmail.com <nejakyusername@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-07-11 09:23:59 +0000

    Fixed problem setting ACL entries
    git-svn-id: https://acris.googlecode.com/svn@6876 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- trunk/acris-security/acris-security-core/src/main/java/sk/seges/acris/security/server/acl/dao/IAclObjectIdentityDao.java
+++ trunk/acris-security/acris-security-core/src/main/java/sk/seges/acris/security/server/acl/dao/IAclObjectIdentityDao.java
@@ -3,13 +3,11 @@ package sk.seges.acris.security.server.acl.dao;
 import java.util.List;
 
 import sk.seges.acris.security.server.core.acl.domain.api.AclSecuredObjectIdentity;
-import sk.seges.acris.security.shared.domain.ISecuredObject;
 import sk.seges.sesam.dao.ICrudDAO;
 
 public interface IAclObjectIdentityDao<T extends AclSecuredObjectIdentity> extends ICrudDAO<T> {
     
 	AclSecuredObjectIdentity findByObjectId(long objectIdClass, long objectIdIdentity);
-	AclSecuredObjectIdentity findByObjectId(Class<? extends ISecuredObject<?>> objectClass, long objectIdIdentity);
 
 	AclSecuredObjectIdentity findById(long id);
 
