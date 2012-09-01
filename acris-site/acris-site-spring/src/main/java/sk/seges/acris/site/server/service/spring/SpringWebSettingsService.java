5714622a27dbc58171aff5c1a1b955760e35b992
nejakyusername@gmail.com <nejakyusername@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-09-01 12:36:27 +0000

    Server side classes in server packages, client side classes in client packages ... webSettings cleanup (required for offline generator)
    git-svn-id: https://acris.googlecode.com/svn/trunk@6932 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-site/acris-site-spring/src/main/java/sk/seges/acris/site/server/service/spring/SpringWebSettingsService.java
+++ acris-site/acris-site-spring/src/main/java/sk/seges/acris/site/server/service/spring/SpringWebSettingsService.java
@@ -4,10 +4,10 @@ import javax.validation.Valid;
 
 import org.springframework.transaction.annotation.Transactional;
 
-import sk.seges.acris.domain.shared.domain.ftp.server.model.data.FTPWebSettingsData;
 import sk.seges.acris.site.server.dao.IWebSettingsDao;
+import sk.seges.acris.site.server.domain.api.server.model.data.WebSettingsData;
+import sk.seges.acris.site.server.domain.ftp.server.model.data.FTPWebSettingsData;
 import sk.seges.acris.site.server.service.WebSettingsService;
-import sk.seges.acris.site.shared.domain.api.server.model.data.WebSettingsData;
 import sk.seges.corpis.server.service.ICountryService;
 import sk.seges.corpis.service.annotation.TransactionPropagation;
 import sk.seges.corpis.service.annotation.TransactionPropagation.PropagationType;
