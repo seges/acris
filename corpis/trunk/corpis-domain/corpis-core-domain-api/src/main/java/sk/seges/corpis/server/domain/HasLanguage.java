7280769e077ebefeb4b18c288586cbd4e94d574e
sloboda@seges.sk <sloboda@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-09-05 16:08:03 +0000

    Add field
    git-svn-id: https://acris.googlecode.com/svn@6948 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- corpis/trunk/corpis-domain/corpis-core-domain-api/src/main/java/sk/seges/corpis/server/domain/HasLanguage.java
+++ corpis/trunk/corpis-domain/corpis-core-domain-api/src/main/java/sk/seges/corpis/server/domain/HasLanguage.java
@@ -9,6 +9,8 @@ package sk.seges.corpis.server.domain;
 //@BeanWrapper
 public interface HasLanguage {
 
+	public static final String LANGUAGE = "language";
+	
 	String getLanguage();
 
 	void setLanguage(String language);
