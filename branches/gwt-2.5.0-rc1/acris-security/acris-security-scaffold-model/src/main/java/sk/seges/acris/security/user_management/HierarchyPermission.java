ae243a3ecdba51953ab464336eaf039b2f9fd8f0
nejakyusername@gmail.com <nejakyusername@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-11-27 11:48:04 +0000

    security mega cleanup - rework to processors, cleanup, migration to GWT2.5
    git-svn-id: https://acris.googlecode.com/svn@7024 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ branches/gwt-2.5.0-rc1/acris-security/acris-security-scaffold-model/src/main/java/sk/seges/acris/security/user_management/HierarchyPermission.java
@@ -0,0 +1,26 @@
+package sk.seges.acris.security.user_management;
+
+import java.io.Serializable;
+import java.util.List;
+
+import sk.seges.corpis.appscaffold.shared.annotation.BaseObject;
+import sk.seges.corpis.appscaffold.shared.annotation.DomainInterface;
+import sk.seges.corpis.server.domain.HasWebId;
+import sk.seges.sesam.domain.IDomainObject;
+
+@DomainInterface
+@BaseObject
+interface HierarchyPermission extends Serializable, IDomainObject<Integer>, HasWebId {
+ 
+	String permission();
+
+	List<HierarchyPermission> childPermissions();
+
+	Integer level();
+
+	Boolean hasChildren();
+
+	HierarchyPermission parent();
+
+	String discriminator();
+}
