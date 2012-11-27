24315932ea674b4a39cbeaba708df9603ccecd91
nejakyusername@gmail.com <nejakyusername@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-11-27 11:41:39 +0000

    security mega cleanup - rework to processors, cleanup, migration to GWT2.5
    git-svn-id: https://acris.googlecode.com/svn@7020 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ branches/gwt-2.5.0-rc1/acris-security/acris-security-dto/src/main/java/sk/seges/acris/security/server/acl/model/dto/configuration/AclSecuredClassDescriptionDTOConfiguration.java
@@ -0,0 +1,18 @@
+package sk.seges.acris.security.server.acl.model.dto.configuration;
+
+import sk.seges.acris.core.client.rpc.IDataTransferObject;
+import sk.seges.acris.security.acl.server.model.data.AclSecuredClassDescriptionData;
+import sk.seges.acris.security.core.server.acl.domain.jpa.JpaAclSecuredClassDescription;
+import sk.seges.sesam.pap.model.annotation.GenerateEquals;
+import sk.seges.sesam.pap.model.annotation.GenerateHashcode;
+import sk.seges.sesam.pap.model.annotation.TransferObjectMapping;
+
+@TransferObjectMapping(domainClass = JpaAclSecuredClassDescription.class)
+@GenerateEquals(generate = false)
+@GenerateHashcode(generate = false)
+public interface AclSecuredClassDescriptionDTOConfiguration extends IDataTransferObject {
+
+	@TransferObjectMapping(domainClass = AclSecuredClassDescriptionData.class, configuration = AclSecuredClassDescriptionDTOConfiguration.class)
+	public interface AclClassDescriptionConfiguration {}
+
+}
