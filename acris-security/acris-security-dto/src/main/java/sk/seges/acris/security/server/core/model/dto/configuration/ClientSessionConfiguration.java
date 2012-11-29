b144183970770525f6901409f1e8ca50b7e1aeed
nejakyusername@gmail.com <nejakyusername@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-11-29 19:04:38 +0000

    Client session configuration
    git-svn-id: https://acris.googlecode.com/svn/branches/gwt-2.5.0-rc1@7040 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-security/acris-security-dto/src/main/java/sk/seges/acris/security/server/core/model/dto/configuration/ClientSessionConfiguration.java
+++ acris-security/acris-security-dto/src/main/java/sk/seges/acris/security/server/core/model/dto/configuration/ClientSessionConfiguration.java
@@ -1,7 +1,13 @@
 package sk.seges.acris.security.server.core.model.dto.configuration;
 
 import sk.seges.acris.security.shared.session.ClientSession;
+import sk.seges.sesam.pap.model.annotation.Ignore;
 import sk.seges.sesam.pap.model.annotation.TransferObjectMapping;
 
 @TransferObjectMapping(domainClass = ClientSession.class, dtoClass = ClientSession.class)
-public interface ClientSessionConfiguration {}
+public interface ClientSessionConfiguration {
+	
+	@Ignore
+	void session();
+
+}
