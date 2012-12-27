f0973271c1a15c1496daa90c1c6795f60267d334
nejakyusername@gmail.com <nejakyusername@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-12-27 20:49:59 +0000

    shared session is not propagated to the DTO
    git-svn-id: https://acris.googlecode.com/svn@7113 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- branches/gwt-2.5.0-rc1/acris-security/acris-security-dto/src/main/java/sk/seges/acris/security/server/core/model/dto/configuration/ClientSessionConfiguration.java
+++ branches/gwt-2.5.0-rc1/acris-security/acris-security-dto/src/main/java/sk/seges/acris/security/server/core/model/dto/configuration/ClientSessionConfiguration.java
@@ -1,13 +1,7 @@
 package sk.seges.acris.security.server.core.model.dto.configuration;
 
 import sk.seges.acris.security.shared.session.ClientSession;
-import sk.seges.sesam.pap.model.annotation.Ignore;
 import sk.seges.sesam.pap.model.annotation.TransferObjectMapping;
 
 @TransferObjectMapping(domainClass = ClientSession.class, dtoClass = ClientSession.class)
-public interface ClientSessionConfiguration {
-	
-	@Ignore
-	void session();
-
-}
+public interface ClientSessionConfiguration {}
