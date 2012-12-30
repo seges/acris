00bbdeaa0612353925a55ccc4d57146bd97c3407
nejakyusername@gmail.com <nejakyusername@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-12-30 21:13:32 +0000

    Validation fix - annotations weren't copied
    git-svn-id: https://acris.googlecode.com/svn@7118 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- sesam/branches/gwt_25/sesam-annotation-processors/sesam-model-domain-processor/src/main/java/sk/seges/sesam/pap/model/printer/copy/CopyPrinter.java
+++ sesam/branches/gwt_25/sesam-annotation-processors/sesam-model-domain-processor/src/main/java/sk/seges/sesam/pap/model/printer/copy/CopyPrinter.java
@@ -1,5 +1,6 @@
 package sk.seges.sesam.pap.model.printer.copy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.lang.model.element.AnnotationMirror;
@@ -30,14 +31,24 @@ public abstract class CopyPrinter extends AbstractElementPrinter {
 		
 		CopyAccessor methodCopyAccessor = new CopyAccessor(context.getDtoMethod(), processingEnv);
 		
-		List<AnnotationMirror> supportedAnnotations = typeCopyAccessor.getSupportedAnnotations(context.getDomainMethod());
-		supportedAnnotations.addAll(typeCopyAccessor.getSupportedAnnotations(MethodHelper.getField(context.getDomainMethod())));
+		List<AnnotationMirror> supportedAnnotations = new ArrayList<AnnotationMirror>();
 
-		supportedAnnotations.addAll(methodCopyAccessor.getSupportedAnnotations(context.getDomainMethod()));
-		supportedAnnotations.addAll(methodCopyAccessor.getSupportedAnnotations(MethodHelper.getField(context.getDomainMethod())));
-
-		supportedAnnotations.addAll(methodCopyAccessor.getSupportedAnnotations(context.getDtoMethod()));
+		add(supportedAnnotations, methodCopyAccessor.getSupportedAnnotations(context.getDomainMethod()));
+		add(supportedAnnotations, methodCopyAccessor.getSupportedAnnotations(MethodHelper.getField(context.getDomainMethod())));
+		add(supportedAnnotations, methodCopyAccessor.getSupportedAnnotations(context.getDtoMethod()));
+		
+		add(supportedAnnotations, typeCopyAccessor.getSupportedAnnotations(context.getDomainMethod()));
+		add(supportedAnnotations, typeCopyAccessor.getSupportedAnnotations(MethodHelper.getField(context.getDomainMethod())));
+		add(supportedAnnotations, typeCopyAccessor.getSupportedAnnotations(context.getDtoMethod()));
 
 		return supportedAnnotations;
+	}
+	
+	private void add(List<AnnotationMirror> annotations, List<AnnotationMirror> copiedAnnotations) {
+		for (AnnotationMirror annotation: copiedAnnotations) {
+			if (!annotations.contains(annotation)) {
+				annotations.add(annotation);
+			}
+		}
 	}
 }
