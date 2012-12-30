311cc33d510d65753ddc68bd14e6d188fed80079
nejakyusername@gmail.com <nejakyusername@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2012-12-30 01:10:17 +0000

    minor tuning in selenium tests + using most latest version of webdriver
    git-svn-id: https://acris.googlecode.com/svn/sesam/branches/gwt_25@7114 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- sesam-selenium-support/src/main/java/sk/seges/sesam/core/test/webdriver/support/StateHolder.java
+++ sesam-selenium-support/src/main/java/sk/seges/sesam/core/test/webdriver/support/StateHolder.java
@@ -6,6 +6,9 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Wait;
+
+import com.google.common.base.Function;
 
 public class StateHolder {
 	
@@ -13,10 +16,12 @@ public class StateHolder {
 	private final WebDriver webDriver;
 
 	private List<WebElement> webElements;
+	private final Wait<WebDriver> wait;
 	
-	public StateHolder(WebDriver webDriver, By selector) {
+	public StateHolder(Wait<WebDriver> wait, WebDriver webDriver, By selector) {
 		this.selector = selector;
 		this.webDriver = webDriver;
+		this.wait = wait;
 	}
 
 	public StateHolder rememberState() {
@@ -27,14 +32,19 @@ public class StateHolder {
 	}
 
 	public WebElement getLastElement() {
-		webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		int size = webElements.size();
-		webElements = webDriver.findElements(selector);
-		webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		final int size = webElements.size();
 		
-		if (webElements.size() <= size) {
-			return null;
-		}
+		wait.until(new Function<WebDriver, Boolean>() {
+
+			@Override
+			public Boolean apply(WebDriver arg0) {
+				webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+				webElements = webDriver.findElements(selector);
+				webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+				return webElements.size() > size;
+			}
+		});
 		
 		return webElements.get(size);
 	}
