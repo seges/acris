4c4d18bfd99d224c21c306719e0752b604e08663
nejakyusername@gmail.com <nejakyusername@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2013-01-23 21:52:41 +0000

    Collections handling - fix for persistent bags
    git-svn-id: https://acris.googlecode.com/svn@7185 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- corpis/branches/gwt_25/corpis-annotation-supports/corpis-domain-model-hibernate-support/src/main/java/sk/seges/corpis/pap/model/hibernate/HibernatePersistentElementHolderConverter.java
+++ corpis/branches/gwt_25/corpis-annotation-supports/corpis-domain-model-hibernate-support/src/main/java/sk/seges/corpis/pap/model/hibernate/HibernatePersistentElementHolderConverter.java
@@ -1,6 +1,7 @@
 package sk.seges.corpis.pap.model.hibernate;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -28,10 +29,12 @@ public class HibernatePersistentElementHolderConverter implements ElementHolderT
 		collectionMappings.put(List.class, ArrayList.class);
 		collectionMappings.put(Map.class, HashMap.class);
 		collectionMappings.put(Set.class, HashSet.class);
+		collectionMappings.put(Collection.class, ArrayList.class);
 	}
 
 	@Override
 	public TypeMirror getIterableDtoType(MutableTypeMirror collectionType) {
+		//TODO handle if the collection is instance of PersistentCollection
 		for (Entry<Class<?>, Class<?>> collections : collectionMappings.entrySet()) {
 			if (processingEnv.getTypeUtils().implementsType(collectionType, processingEnv.getTypeUtils().toMutableType(collections.getKey()))) {
 				TypeElement typeElement = processingEnv.getElementUtils().getTypeElement(collections.getValue().getCanonicalName());
