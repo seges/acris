fb266bd9cfe2e963712417853e744b8b02014682
sloboda@seges.sk <sloboda@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2013-02-11 13:01:14 +0000

    Add order status type, update enum for OrderStatus
    git-svn-id: https://acris.googlecode.com/svn@7222 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- corpis/branches/gwt_25/corpis-domain/corpis-invoice-domain-model/src/main/java/sk/seges/corpis/server/domain/invoice/OrderStatus.java
+++ corpis/branches/gwt_25/corpis-domain/corpis-invoice-domain-model/src/main/java/sk/seges/corpis/server/domain/invoice/OrderStatus.java
@@ -6,6 +6,7 @@ import sk.seges.corpis.appscaffold.shared.annotation.BaseObject;
 import sk.seges.corpis.appscaffold.shared.annotation.DomainInterface;
 import sk.seges.corpis.server.domain.HasWebId;
 import sk.seges.corpis.server.domain.Name;
+import sk.seges.corpis.shared.domain.invoice.EOrderStatus;
 import sk.seges.sesam.domain.IMutableDomainObject;
 
 @DomainInterface
@@ -15,4 +16,5 @@ public interface OrderStatus extends IMutableDomainObject<Long>, HasWebId{
 	String webId();
 	List<Name> names();
 	Integer index();
+	EOrderStatus type();
 }
--- corpis/branches/gwt_25/corpis-domain/corpis-invoice-domain-model/src/main/java/sk/seges/corpis/shared/domain/invoice/EOrderStatus.java
+++ corpis/branches/gwt_25/corpis-domain/corpis-invoice-domain-model/src/main/java/sk/seges/corpis/shared/domain/invoice/EOrderStatus.java
@@ -1,9 +1,14 @@
 package sk.seges.corpis.shared.domain.invoice;
 
 public enum EOrderStatus {
-	NEW,			/* Order is just created */
-	PROCESSING,		/* Order is currently processed somehow - like moving from external stock, manufactured, etc */
-	PROCESSED,		/* Order is ready to dispatch, or waiting for payment */
-	CANCELLED,		/* Order is cancelled */
-	DISPATCHED;		/* Order is dispatched */
+	NEW,			/* idx1 - Order is just created */
+	PROCESSING,		/* idx4 - Order is currently processed somehow - like moving from external stock, manufactured, etc */
+	PROCESSED,		/* idx5 - Order is ready to dispatch, or waiting for payment */
+	CANCELLED,		/* idx9 - Order is cancelled */
+	DISPATCHED,		/* idxX - Order is dispatched */
+	ACCEPTED,		/* idx2 - Order is accepted by shop administrator */
+	NOT_ACCEPTED,	/* idx9 - Order was not accepted by shop administrator */
+	PAID,			/* idx3 - Order was paid */
+	DELIVERED,		/* idx9 - Order was delivered to the customer */
+	WAITING;		/* idxX - Order is waiting for products availability */
 }
