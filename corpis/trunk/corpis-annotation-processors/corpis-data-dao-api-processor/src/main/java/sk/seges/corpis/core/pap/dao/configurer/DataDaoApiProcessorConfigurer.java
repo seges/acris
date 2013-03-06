bab485d8ae99db4a7e1cc5964b9866eb90732ff6
nejakyusername@gmail.com <nejakyusername@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2013-03-06 17:46:49 +0000

    trunk removed
    git-svn-id: https://acris.googlecode.com/svn@7279 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- corpis/trunk/corpis-annotation-processors/corpis-data-dao-api-processor/src/main/java/sk/seges/corpis/core/pap/dao/configurer/DataDaoApiProcessorConfigurer.java
+++
@@ -1,7 +0,0 @@
-package sk.seges.corpis.core.pap.dao.configurer;
-
-import javax.annotation.processing.SupportedSourceVersion;
-import javax.lang.model.SourceVersion;
-
-@SupportedSourceVersion(SourceVersion.RELEASE_6)
-public class DataDaoApiProcessorConfigurer extends AbstractDaoApiConfigurer {}
