fd775cafd6a35fe047473e20de513cd08a178b3e
sloboda@seges.sk <sloboda@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2013-03-25 11:52:59 +0000

    git-svn-id: https://acris.googlecode.com/svn@7291 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- corpis/trunk/corpis-annotation-processors/corpis-service-processor/src/main/java/sk/seges/corpis/core/server/service/api/Finder.java
+++
@@ -1,5 +0,0 @@
-package sk.seges.corpis.core.server.service.api;
-
-public interface Finder<T> {
-	boolean accept(T t);
-}
--- corpis/trunk/corpis-domain/corpis-core-domain-api/src/main/java/sk/seges/corpis/server/domain/HasName.java
+++
@@ -1,8 +0,0 @@
-package sk.seges.corpis.server.domain;
-
-public interface HasName {
-
-	String getName();
-
-	void setName(String name);
-}
