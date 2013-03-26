749bf099dfd58bd0ecd266e36258402085e02625
sloboda@seges.sk <sloboda@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2013-03-26 14:44:15 +0000

    git-svn-id: https://acris.googlecode.com/svn@7302 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- corpis/trunk/corpis-domain/corpis-price-domain-api/src/main/java/sk/seges/corpis/shared/domain/price/api/IPriceCondition.java
+++
@@ -1,9 +0,0 @@
-package sk.seges.corpis.shared.domain.price.api;
-
-import sk.seges.corpis.server.domain.customer.Customer;
-import sk.seges.corpis.server.domain.invoice.Product;
-
-
-public interface IPriceCondition {
-	public boolean applies(PriceConditionContext context, String webId, Customer customer, Product product);
-}
