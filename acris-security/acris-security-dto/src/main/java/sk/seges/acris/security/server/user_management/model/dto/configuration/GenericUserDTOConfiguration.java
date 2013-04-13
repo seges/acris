e1e6ec16568d3dd3892e896b4dbcc404efaf3245
nejakyusername@gmail.com <nejakyusername@gmail.com@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2013-04-13 16:25:02 +0000

    DTO configurations
    git-svn-id: https://acris.googlecode.com/svn/branches/gwt-2.5.0-rc1@7333 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- acris-security/acris-security-dto/src/main/java/sk/seges/acris/security/server/user_management/model/dto/configuration/GenericUserDTOConfiguration.java
+++ acris-security/acris-security-dto/src/main/java/sk/seges/acris/security/server/user_management/model/dto/configuration/GenericUserDTOConfiguration.java
@@ -2,7 +2,6 @@ package sk.seges.acris.security.server.user_management.model.dto.configuration;
 
 import sk.seges.acris.core.client.rpc.IDataTransferObject;
 import sk.seges.acris.security.shared.core.user_management.domain.hibernate.HibernateGenericUser;
-import sk.seges.acris.security.user_management.server.model.data.UserData;
 import sk.seges.sesam.pap.model.annotation.GenerateEquals;
 import sk.seges.sesam.pap.model.annotation.GenerateHashcode;
 import sk.seges.sesam.pap.model.annotation.Mapping;
@@ -14,9 +13,6 @@ import sk.seges.sesam.pap.model.annotation.TransferObjectMapping;
 @GenerateEquals(generate = false)
 @GenerateHashcode(generate = false)
 public interface GenericUserDTOConfiguration extends IDataTransferObject {
-
-	@TransferObjectMapping(domainClass = UserData.class, configuration = GenericUserDTOConfiguration.class)
-	public interface UserDataConfiguration {}
 
 	void id();
 	void userAuthorities();
--- acris-security/acris-security-dto/src/main/java/sk/seges/acris/security/server/user_management/model/dto/configuration/UserRoleDTOConfiguration.java
+++ acris-security/acris-security-dto/src/main/java/sk/seges/acris/security/server/user_management/model/dto/configuration/UserRoleDTOConfiguration.java
@@ -1,11 +1,11 @@
 package sk.seges.acris.security.server.user_management.model.dto.configuration;
 
-import sk.seges.acris.security.server.core.user_management.domain.jpa.JpaSecurityRole;
+import sk.seges.acris.security.shared.core.user_management.domain.hibernate.HibernateSecurityRole;
 import sk.seges.sesam.pap.model.annotation.GenerateEquals;
 import sk.seges.sesam.pap.model.annotation.GenerateHashcode;
 import sk.seges.sesam.pap.model.annotation.TransferObjectMapping;
 
-@TransferObjectMapping(domainClass = JpaSecurityRole.class)
+@TransferObjectMapping(domainClass = HibernateSecurityRole.class)
 @GenerateEquals(generate = false)
 @GenerateHashcode(generate = false)
 public interface UserRoleDTOConfiguration {}
