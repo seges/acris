4cd13fe781cb8a079abd700b8473109da57e03e3
sloboda@seges.sk <sloboda@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2013-05-09 11:45:33 +0000

    copy to trunk
    git-svn-id: https://acris.googlecode.com/svn@7356 e28c4d36-3818-11de-97fb-aff7d2a6cc54

---
+++ trunk/acris-widgets/src/main/java/sk/seges/acris/widget/client/optionpane/listener/IOptionsListener.java
@@ -0,0 +1,7 @@
+package sk.seges.acris.widget.client.optionpane.listener;
+
+import sk.seges.acris.widget.client.optionpane.EPanelResult;
+
+public interface IOptionsListener {
+	void optionSelected(EPanelResult result);
+}
