d82850e9e8e7fd6f5f0f5fe01f7893f52d7ac645
sloboda@seges.sk <sloboda@seges.sk@e28c4d36-3818-11de-97fb-aff7d2a6cc54>
2013-06-04 13:11:58 +0000

    Add new openId provider + log status message
    git-svn-id: https://acris.googlecode.com/svn@7382 e28c4d36-3818-11de-97fb-aff7d2a6cc54

--- branches/gwt-2.5.0-rc1/acris-security/acris-security-openid/acris-security-openid-scaffold-model/src/main/java/sk/seges/acris/security/shared/user_management/domain/api/OpenIDProvider.java
+++ branches/gwt-2.5.0-rc1/acris-security/acris-security-openid/acris-security-openid-scaffold-model/src/main/java/sk/seges/acris/security/shared/user_management/domain/api/OpenIDProvider.java
@@ -1,5 +1,5 @@
 package sk.seges.acris.security.shared.user_management.domain.api;
 
 public enum OpenIDProvider {
-	GOOGLE, YAHOO, AOL, SEZNAM, MYOPENID;
+	GOOGLE, YAHOO, AOL, SEZNAM, MYOPENID, SCALEDO;
 }
