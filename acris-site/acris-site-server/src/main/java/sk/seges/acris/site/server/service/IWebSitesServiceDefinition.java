051a719d9d7e45a92ed44ea8cc8b0149ec0b062c
unknown <peto-sloboda@.(none)>
2013-07-15 17:10:36 +0200

    #5932 - remove vhosts

--- acris-site/acris-site-server/src/main/java/sk/seges/acris/site/server/service/IWebSitesServiceDefinition.java
+++ acris-site/acris-site-server/src/main/java/sk/seges/acris/site/server/service/IWebSitesServiceDefinition.java
@@ -1,12 +1,14 @@
 package sk.seges.acris.site.server.service;
 
-import sk.seges.acris.site.server.model.data.WebSitesData;
-
 import java.util.List;
+
+import sk.seges.acris.site.server.model.data.WebSitesData;
 
 public interface IWebSitesServiceDefinition extends IWebSitesServiceLocal {
 
 	List<WebSitesData> findWebSitesByDomain(String domain);
 
 	void deleteWebSites(WebSitesData webSite);
+	
+	void deleteWebSites(String webId, String domain);	
 }
