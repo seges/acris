a3b333c6d9125bac2e8b2e4e6b3b9442c1d74d1b
peter_simun <simun@seges.sk>
2014-02-17 00:31:48 +0100

    [0006791] - prihlasovanie do newslettra nejde

--- acris-site/acris-site-api/src/main/java/sk/seges/acris/site/shared/domain/api/SubscriptionFormat.java
+++ acris-site/acris-site-api/src/main/java/sk/seges/acris/site/shared/domain/api/SubscriptionFormat.java
@@ -3,9 +3,11 @@
  */
 package sk.seges.acris.site.shared.domain.api;
 
+import sk.seges.sesam.shared.model.api.TransferableEnum;
+
 /**
  * @author eldzi
  */
-public enum SubscriptionFormat {
+public enum SubscriptionFormat implements TransferableEnum {
 	PLAIN, HTML, PLAIN_AND_HTML;
 }
