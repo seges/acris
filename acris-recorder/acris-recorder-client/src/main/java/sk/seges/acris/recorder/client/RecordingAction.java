d2c80748d5f5a79b770065dac70ac34bd175a9a6
peter_simun <simun@seges.sk>
2014-03-23 17:08:45 +0100

    compilation fix

--- acris-recorder/acris-recorder-client/src/main/java/sk/seges/acris/recorder/client/RecordingAction.java
+++ acris-recorder/acris-recorder-client/src/main/java/sk/seges/acris/recorder/client/RecordingAction.java
@@ -1,9 +1,7 @@
 package sk.seges.acris.recorder.client;
 
-import sk.seges.acris.recorder.client.recorder.BatchRecorder;
-import sk.seges.acris.recorder.client.ui.RecorderUI;
-
 import com.google.gwt.core.client.EntryPoint;
+import sk.seges.acris.recorder.client.recorder.BatchRecorder;
 
 public class RecordingAction implements EntryPoint {
 
