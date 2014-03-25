a5c722b379e12cee9d2a3f34313ef9a386906784
peter_simun <simun@seges.sk>
2014-03-25 01:23:27 +0100

    using shared package instead of rpc

---
+++ acris-client-core/src/main/java/sk/seges/acris/core/shared/model/IDataTransferObject.java
@@ -0,0 +1,14 @@
+package sk.seges.acris.core.shared.model;
+
+import java.io.Serializable;
+
+/**
+ * Common interface for all DTO objects. All generators are related to this
+ * interface. 
+ * 
+ * @author fat
+ * 
+ */
+public interface IDataTransferObject extends Serializable {
+
+}
