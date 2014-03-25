30fd6ca520b1f8f7143c9e286ba28c193daf8111
peter_simun <simun@seges.sk>
2014-03-25 01:21:06 +0100

    #6986 compilation fixes, proper GWT modules inherit

--- acris-recorder/acris-recorder-client/src/main/java/sk/seges/acris/recorder/client/event/AnimationEvent.java
+++
@@ -1,5 +0,0 @@
-package sk.seges.acris.recorder.client.event;
-
-public interface AnimationEvent {
-	void onComplete();
-}
--- acris-recorder/acris-recorder-client/src/main/java/sk/seges/acris/recorder/client/listener/EventListener.java
+++
@@ -1,8 +0,0 @@
-package sk.seges.acris.recorder.client.listener;
-
-public interface EventListener {
-
-	void onFailure();
-
-	void onSuccess();
-}
