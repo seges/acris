792ea0cdb929d598eae0625fcfabb0c862d552d6
Jaroslav Smolinský <jaroslav.smolinsky@seges.sk>
2014-08-13 20:55:52 +0200

    #7423 rozsirenie statistik * [DONE] datumy formatovat rovnako ... teraz je 2014.03.01 18:44:53 vs 09.08.2014 06:58:14 * [DONE] site url brat z web_sites ** pozor: moze byt viac domen, pre rozne locale, s rovnakym webid ** zatial zobrazovat spolu ako jeden zaznam, doriesi sa neskor -> #7544 * [DONE] reuqestIp nemat ako zvalst atribut, premiestnit do paramsov

--- acris-site/acris-site-server/src/main/java/sk/seges/acris/site/server/dao/IWebSitesDao.java
+++ acris-site/acris-site-server/src/main/java/sk/seges/acris/site/server/dao/IWebSitesDao.java
@@ -1,11 +1,18 @@
 package sk.seges.acris.site.server.dao;
 
+import java.util.List;
+
+import org.hibernate.criterion.DetachedCriteria;
+
 import sk.seges.acris.site.server.model.data.WebSitesData;
 import sk.seges.sesam.dao.ICrudDAO;
+import sk.seges.sesam.dao.Page;
 
 public interface IWebSitesDao <T extends WebSitesData> extends ICrudDAO<T> {
 
 	WebSitesData createDefaultEntity();
 	
 	WebSitesData loadWebSitesByDomain(String domain);
+	
+	List<WebSitesData> findByCriteria(DetachedCriteria criteria, Page allResultsPage);
 }
