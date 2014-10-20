7cde8adb16798f94a3e7d1f4a78df675c13a5a02
simun@seges.sk <simun@seges.sk>
2014-10-20 23:41:33 +0200

    #5497 Added support for the JDK7 - com.sun classes replaced by other ones

--- acris-image-utils/src/main/java/sk/seges/acris/server/image/loader/JpegImageLoader.java
+++ acris-image-utils/src/main/java/sk/seges/acris/server/image/loader/JpegImageLoader.java
@@ -1,18 +1,24 @@
 package sk.seges.acris.server.image.loader;
 
+import org.apache.commons.imaging.ImageInfo;
+import org.apache.commons.imaging.Imaging;
+import sk.seges.acris.server.image.model.ColorType;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
 
-import sk.seges.acris.server.image.model.ColorType;
-
-import com.sun.image.codec.jpeg.ImageFormatException;
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGImageDecoder;
-
+/**
+ * Sources:
+ * https://docs.atlassian.com/atlassian-core/4.6.11/xref/com/atlassian/core/util/thumbnail/loader/CMYKImageLoader.html
+ * http://stackoverflow.com/questions/3123574/how-to-convert-from-cmyk-to-rgb-in-java-correctly
+ */
 public abstract class JpegImageLoader extends ProcessingImageLoader {
 
 	protected final File file;
@@ -22,53 +28,38 @@ public abstract class JpegImageLoader extends ProcessingImageLoader {
 	}
 	
 	static ColorType getJpegColorType(File file) {
-		
-		JPEGImageDecoder decoder;
-		FileInputStream fileInputStream;
-		try {
-			fileInputStream = new FileInputStream(file);
-			decoder = JPEGCodec.createJPEGDecoder(fileInputStream);
-		} catch (FileNotFoundException e1) {
-			return null;
-		}
-		
-		try {
-			decoder.decodeAsRaster();
-		} catch (ImageFormatException e) {
-			return ColorType.RGB;
-		} catch (IOException e) {
-			return ColorType.RGB;
-		}
-		
-		int colorType = decoder.getJPEGDecodeParam().getEncodedColorID();	
-		
-		try {
-			fileInputStream.close();
-		} catch (IOException e) {
-			return null;
-		}
-		
+
+        int colorType;
+
+        try {
+            colorType = Imaging.getImageInfo(file).getColorType();
+        } catch (Exception e) {
+            return null;
+        }
+
 		switch (colorType) {
-			case 4:
+			case ImageInfo.COLOR_TYPE_CMYK:
 				return ColorType.CMYK;
-			case 7:
+			case ImageInfo.COLOR_TYPE_YCCK:
 				return ColorType.YCCK;
-			default:
-				return ColorType.RGB;
+            default:
+//            case ImageInfo.COLOR_TYPE_RGB:
+                return ColorType.RGB;
+//			default:
+//				throw new RuntimeException("Unsupported image type (" + colorType + ") " + file.getAbsolutePath());
 		}
 	}
 
 	@Override
 	public BufferedImage getBufferedImage() {
 		Raster srcRaster;
-		
-		BufferedImage ret;
+
 		FileInputStream fileInputStream;
-		
+        BufferedImage ret;
+
 		try {
 			fileInputStream = new FileInputStream(file);
-			JPEGImageDecoder decoder = JPEGCodec.createJPEGDecoder(fileInputStream);
-			srcRaster = decoder.decodeAsRaster();
+			srcRaster = getRasterFromStream(fileInputStream);
 
 			ret = new BufferedImage(srcRaster.getWidth(), srcRaster.getHeight(), BufferedImage.TYPE_INT_RGB);
 		} catch (Exception ex) {
@@ -83,4 +74,30 @@ public abstract class JpegImageLoader extends ProcessingImageLoader {
 		
 		return ret;
 	}
+
+    private Raster getRasterFromStream(InputStream inputStream) throws IOException {
+
+        Iterator<ImageReader> readers = ImageIO.getImageReadersByFormatName("JPEG");
+        ImageReader reader = null;
+        try {
+            while (readers.hasNext()) {
+                reader = readers.next();
+                if (reader.canReadRaster()) {
+                    break;
+                } else {
+                    reader.dispose();
+                    reader = null;
+                }
+            }
+            if (reader == null) {
+                return null;
+            }
+            reader.setInput(ImageIO.createImageInputStream(inputStream));
+            return reader.readRaster(0, null);
+        } finally {
+            if (reader != null) {
+                reader.dispose();
+            }
+        }
+    }
 }
