1698f37438338e9043348a33f9f4379f6abee34f
Peter Sloboda <sloboda@seges.sk>
2015-07-13 16:29:25 +0200

    #8260 Separate constants, prepare new test rest api for apiKey auth

--- acris-security/acris-security-rest-authentication/src/main/java/sk/seges/acris/security/rest/RestAuthenticationResource.java
+++ acris-security/acris-security-rest-authentication/src/main/java/sk/seges/acris/security/rest/RestAuthenticationResource.java
@@ -12,13 +12,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import sk.seges.acris.crypto.util.Hasher;
-import sk.seges.acris.security.server.core.user_management.context.APIKeyUserService;
+import sk.seges.acris.security.server.core.user_management.context.APIKeyConstants;
 
 @Path("/auth")
 public class RestAuthenticationResource {
 	
 	private static final Logger log = LoggerFactory.getLogger(RestAuthenticationResource.class);
 
+	/**
+	 * Return JSON for authentication
+	 * {allowed: “true”/”false”}
+	 */
 	@GET
 	@Path("/user")
 	@Produces({ MediaType.APPLICATION_JSON })
@@ -26,13 +30,39 @@ public class RestAuthenticationResource {
 		JSONObject result = null;
 		try {
 			if (webId != null && (Hasher.getSHAHexDigest(webId).equals(apiKey))) {
-				result = new JSONObject().put(APIKeyUserService.RESULT_PARAMETER, "TRUE");
+				result = new JSONObject().put(APIKeyConstants.RESULT_PARAMETER, "TRUE");
 			} else {
-				result = new JSONObject().put(APIKeyUserService.RESULT_PARAMETER, "FALSE");
+				result = new JSONObject().put(APIKeyConstants.RESULT_PARAMETER, "FALSE");
 			}
 		} catch (JSONException e) {
 			log.error("Could not parse json, cause:" , e);
 		}
 		return result;
 	}
+	
+	/**
+	 * Return JSON with user and password for 
+	 * FTP authentication, where to publish ofline version of the web
+	 * {allowed: “true”/”false”, user: “test_user”,password: “test_pass”}
+	 */
+	@GET
+	@Path("/userftp")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public JSONObject auth(@QueryParam("webId") String webId, @QueryParam("apiKey") String apiKey) {
+		JSONObject result = null;
+		try {
+			if (webId != null && (Hasher.getSHAHexDigest(webId).equals(apiKey))) {
+				result = new JSONObject();
+				result.put(APIKeyConstants.RESULT_PARAMETER, "TRUE");
+				result.put(APIKeyConstants.RESULT_PARAMETER_FTP_USER, "test_user");
+				result.put(APIKeyConstants.RESULT_PARAMETER_FTP_PASS, "test_pass");
+			} else {
+				result = new JSONObject().put(APIKeyConstants.RESULT_PARAMETER, "FALSE");
+			}
+		} catch (JSONException e) {
+			log.error("Could not parse json, cause:" , e);
+		}
+		return result;
+	}
+
 }
